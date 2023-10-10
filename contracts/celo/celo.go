36caac99c51c186e1ea940a4779bccfb051c11ba
Karl Bartel <karl.bartel@clabs.co>
2023-10-10 15:44:30 +0200

    Add more contracts from monorepo

--- contracts/celo/celo.go
+++ contracts/celo/celo.go
@@ -3,9 +3,12 @@ package contracts
 import _ "embed"
 
 //go:generate go run ../../cmd/abigen --pkg abigen --out abigen/GoldToken.go --abi compiled/GoldToken.abi --type GoldToken
-//go:generate go run ../../cmd/abigen --pkg abigen --out abigen/Registry.go --abi compiled/Registry.abi --type Registry
+//go:generate go run ../../cmd/abigen --pkg abigen --out abigen/Registry.go --abi compiled/CeloRegistry.abi --type Registry
+//go:generate go run ../../cmd/abigen --pkg abigen --out abigen/SortedOracles.go --abi compiled/SortedOracles.abi --type SortedOracles
+//go:generate go run ../../cmd/abigen --pkg abigen --out abigen/FeeCurrencyWhitelist.go --abi compiled/FeeCurrencyWhitelist.abi --type FeeCurrencyWhitelist
+//go:generate go run ../../cmd/abigen --pkg abigen --out abigen/FeeCurrency.go --abi compiled/FeeCurrency.abi --type FeeCurrency
 
-//go:embed compiled/Registry.bin-runtime
+//go:embed compiled/CeloRegistry.bin-runtime
 var RegistryBytecodeRaw []byte
 
 //go:embed compiled/GoldToken.bin-runtime
@@ -13,3 +16,15 @@ var GoldTokenBytecodeRaw []byte
 
 //go:embed compiled/Proxy.bin-runtime
 var ProxyBytecodeRaw []byte
+
+//go:embed compiled/SortedOracles.bin-runtime
+var SortedOraclesBytecodeRaw []byte
+
+//go:embed compiled/FeeCurrencyWhitelist.bin-runtime
+var FeeCurrencyWhitelistBytecodeRaw []byte
+
+//go:embed compiled/FeeCurrency.bin-runtime
+var FeeCurrencyBytecodeRaw []byte
+
+//go:embed compiled/AddressSortedLinkedListWithMedian.bin-runtime
+var AddressSortedLinkedListWithMedianBytecodeRaw []byte
