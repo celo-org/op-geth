cc63a526dc598ce5c547d1020ea22a510b3eff97
Paul Lange <palango@gmx.de>
2023-11-10 15:44:13 +0100

    cleanup: Fix signer creation for cel2

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -40,6 +40,8 @@ type sigCache struct {
 func MakeSigner(config *params.ChainConfig, blockNumber *big.Int, blockTime uint64) Signer {
 	var signer Signer
 	switch {
+	case config.IsCel2(blockTime):
+		signer = NewCel2Signer(config.ChainID)
 	case config.IsCancun(blockNumber, blockTime):
 		signer = NewCancunSigner(config.ChainID)
 	case config.IsLondon(blockNumber):
