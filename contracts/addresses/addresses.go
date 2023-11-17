659b714076cdb9df8c9cc2a57ead9fe1142c6318
Karl Bartel <karl.bartel@clabs.co>
2023-11-17 11:06:19 +0100

    contracts: Add celo core contract addresses

---
+++ contracts/addresses/addresses.go
@@ -0,0 +1,71 @@
+package addresses
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/params"
+)
+
+type CeloAddresses struct {
+	CeloToken            common.Address
+	FeeHandler           common.Address
+	FeeCurrencyDirectory common.Address
+}
+
+var (
+	MainnetAddresses = &CeloAddresses{
+		CeloToken:            common.HexToAddress("0x471ece3750da237f93b8e339c536989b8978a438"),
+		FeeHandler:           common.HexToAddress("0xcd437749e43a154c07f3553504c68fbfd56b8778"),
+		FeeCurrencyDirectory: common.HexToAddress("0x15F344b9E6c3Cb6F0376A36A64928b13F62C6276"),
+	}
+
+	AlfajoresAddresses = &CeloAddresses{
+		CeloToken:            common.HexToAddress("0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9"),
+		FeeHandler:           common.HexToAddress("0xEAaFf71AB67B5d0eF34ba62Ea06Ac3d3E2dAAA38"),
+		FeeCurrencyDirectory: common.HexToAddress("0x9212Fb72ae65367A7c887eC4Ad9bE310BAC611BF"),
+	}
+
+	BaklavaAddresses = &CeloAddresses{
+		CeloToken:            common.HexToAddress("0xdDc9bE57f553fe75752D61606B94CBD7e0264eF8"),
+		FeeHandler:           common.HexToAddress("0xeed0A69c51079114C280f7b936C79e24bD94013e"),
+		FeeCurrencyDirectory: common.HexToAddress("0xD59E1599F45e42Eb356202B2C714D6C7b734C034"),
+	}
+
+	CeloSepoliaAddresses = &CeloAddresses{
+		CeloToken:            common.HexToAddress("0x471EcE3750Da237f93B8E339c536989b8978a438"),
+		FeeHandler:           common.HexToAddress("0xcD437749E43A154C07F3553504c68fBfD56B8778"),
+		FeeCurrencyDirectory: common.HexToAddress("0x9212Fb72ae65367A7c887eC4Ad9bE310BAC611BF"),
+	}
+)
+
+// GetAddresses returns the addresses for the given chainID or
+// nil if not found.
+func GetAddresses(chainID *big.Int) *CeloAddresses {
+	// ChainID can be uninitialized in some tests
+	if chainID == nil {
+		return nil
+	}
+	switch chainID.Uint64() {
+	case params.CeloAlfajoresChainID:
+		return AlfajoresAddresses
+	case params.CeloBaklavaChainID:
+		return BaklavaAddresses
+	case params.CeloSepoliaChainID:
+		return CeloSepoliaAddresses
+	case params.CeloMainnetChainID:
+		return MainnetAddresses
+	default:
+		return nil
+	}
+}
+
+// GetAddressesOrDefault returns the addresses for the given chainID or
+// the Mainnet addresses if none are found.
+func GetAddressesOrDefault(chainID *big.Int, defaultValue *CeloAddresses) *CeloAddresses {
+	addresses := GetAddresses(chainID)
+	if addresses == nil {
+		return defaultValue
+	}
+	return addresses
+}
