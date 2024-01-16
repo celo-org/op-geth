715f16e2276b3e284b7b642e5ddfc9e39f7ce119
Karl Bartel <karl.bartel@clabs.co>
2024-01-16 15:10:49 +0100

    Formatted with biome + Makefile with format command

--- e2e_test/js-tests/test_ethers_tx.mjs
+++ e2e_test/js-tests/test_ethers_tx.mjs
@@ -1,22 +1,22 @@
-import { assert } from 'chai';
-import 'mocha';
+import { assert } from "chai";
+import "mocha";
 import { ethers } from "ethers";
 
 const provider = new ethers.JsonRpcProvider(process.env.ETH_RPC_URL);
 const signer = new ethers.Wallet(process.env.ACC_PRIVKEY, provider);
 
-describe('ethers.js send tx', () => {
-	it('send basic tx and check receipt', async () => {
-    const tx = await signer.sendTransaction({
-      to: '0x00000000000000000000000000000000DeaDBeef',
-      value: 1
-    });
-    const receipt = await tx.wait();
-  }).timeout(10_000);
+describe("ethers.js send tx", () => {
+	it("send basic tx and check receipt", async () => {
+		const tx = await signer.sendTransaction({
+			to: "0x00000000000000000000000000000000DeaDBeef",
+			value: 1,
+		});
+		const receipt = await tx.wait();
+	}).timeout(10_000);
 });
 
-describe('ethers.js compatibility tests with state', () => {
-	it('provider.getBlock works (block has gasLimit set)', async () => {
+describe("ethers.js compatibility tests with state", () => {
+	it("provider.getBlock works (block has gasLimit set)", async () => {
 		let block = await provider.getBlock();
 
 		// These assertions trigger on undefined or null
@@ -24,7 +24,7 @@ describe('ethers.js compatibility tests with state', () => {
 		assert.notEqual(block.gasLimit, null);
 	});
 
-	it('EIP-1559 transactions supported (can get feeData)', async () => {
+	it("EIP-1559 transactions supported (can get feeData)", async () => {
 		const feeData = await provider.getFeeData();
 
 		// These assertions trigger on undefined or null
@@ -34,19 +34,19 @@ describe('ethers.js compatibility tests with state', () => {
 		assert.notEqual(feeData.gasPrice, null);
 	});
 
-	it('block has gasLimit', async () => {
-		const fullBlock = await provider.send(
-			'eth_getBlockByNumber',
-			['latest', true]
-		)
-		assert.isTrue(fullBlock.hasOwnProperty('gasLimit'))
+	it("block has gasLimit", async () => {
+		const fullBlock = await provider.send("eth_getBlockByNumber", [
+			"latest",
+			true,
+		]);
+		assert.isTrue(fullBlock.hasOwnProperty("gasLimit"));
 	});
 
-	it('block has baseFeePerGas', async () => {
-		const fullBlock = await provider.send(
-			'eth_getBlockByNumber',
-			['latest', true]
-		)
-		assert.isTrue(fullBlock.hasOwnProperty('baseFeePerGas'))
+	it("block has baseFeePerGas", async () => {
+		const fullBlock = await provider.send("eth_getBlockByNumber", [
+			"latest",
+			true,
+		]);
+		assert.isTrue(fullBlock.hasOwnProperty("baseFeePerGas"));
 	});
 });
