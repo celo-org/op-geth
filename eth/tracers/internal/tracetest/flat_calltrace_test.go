5e29fb4ceb0a61bc384847d3d3efa26cf358e220
Paul Lange <palango@gmx.de>
2024-01-31 14:45:25 +0100

    Handle fee currency transactions in state transition

--- eth/tracers/internal/tracetest/flat_calltrace_test.go
+++ eth/tracers/internal/tracetest/flat_calltrace_test.go
@@ -93,7 +93,7 @@ func flatCallTracerTestRunner(tracerName string, filename string, dirPath string
 		return fmt.Errorf("failed to create call tracer: %v", err)
 	}
 
-	msg, err := core.TransactionToMessage(tx, signer, context.BaseFee)
+	msg, err := core.TransactionToMessage(tx, signer, context.BaseFee, context.ExchangeRates)
 	if err != nil {
 		return fmt.Errorf("failed to prepare transaction for tracing: %v", err)
 	}
