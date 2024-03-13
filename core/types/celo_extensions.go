b81f5855b30eb8875172de19b4e6ec24b54a6fde
Piers Powlesland <pierspowlesland@gmail.com>
2024-03-13 11:17:43 +0000

    Handle both celo and op block body decoding

--- core/types/celo_extensions.go
+++ core/types/celo_extensions.go
@@ -1,10 +1,13 @@
 package types
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/rlp"
+	"fmt"
 	"math/big"
 	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 type IstanbulExtra rlp.RawValue
@@ -81,4 +84,83 @@ func (h *Header) DecodeRLP(s *rlp.Stream) error {
 	}
 
 	return err
+}
+
+type CeloBody struct {
+	Transactions []*Transaction
+
+	// These fields were custom additions to the celo block body, but neither
+	// are required in CEL2, so we "skip" decoding them by setting their value
+	// to rlp.RawValue.
+	Randomness     rlp.RawValue
+	EpochSnarkData rlp.RawValue
+}
+
+// This type is required to avoid an infinite loop when decoding
+type opBody Body
+
+func (b *Body) DecodeRLP(s *rlp.Stream) error {
+	// Celo block bodies differ from op block bodies in that the second field
+	// is Randomness which is a struct containing 2 hashes and Randomness is
+	// never nil. We can use this to detect celo block bodies, because in
+	// contrast optimism has a list of Headers as the second field which is
+	// either empty or with one header is far larger than 2 hashes (size 66
+	// each hash is 1 byte specifying the length in bytes and then 32 bytes for
+	// the hash)
+
+	var data rlp.RawValue
+	err := s.Decode(&data)
+	if err != nil {
+		return err
+	}
+	var remaining []byte = data
+
+	kind, tagSize, _, err := rlp.ReadKind(remaining)
+	if err != nil {
+		panic(fmt.Sprintf("Error decoding block body: %v", err))
+	}
+	// At the top level we expect a list
+	if kind != rlp.List {
+		panic(fmt.Sprintf("Expecting encoded body to be an rlp list, instead got %v", kind))
+	}
+	// We skip over the list tag so we can dig into the list data.
+	remaining = remaining[tagSize:]
+
+	kind, tagSize, contentSize, err := rlp.ReadKind(remaining)
+	if err != nil {
+		panic(fmt.Sprintf("Error decoding block body: %v", err))
+	}
+	// Now we expect a list of transactions which we wish to skip over.
+	if kind != rlp.List {
+		panic(fmt.Sprintf("Expecting encoded body to be an rlp list, instead got %v", kind))
+	}
+	remaining = remaining[tagSize+contentSize:]
+
+	// Now we expect either a list of headers or the randomness which will have tagSize 2 and content size 66.
+	kind, tagSize, contentSize, err = rlp.ReadKind(remaining)
+	if err != nil {
+		panic(fmt.Sprintf("Error decoding block body: %v", err))
+	}
+	if kind != rlp.List {
+		panic(fmt.Sprintf("Expecting encoded body to be an rlp list, instead got %v", kind))
+	}
+	// celo block body
+	if tagSize == 2 && contentSize == 66 {
+		body := new(CeloBody)
+		if err := rlp.DecodeBytes(data, body); err != nil {
+			log.Error("Invalid block body 1 RLP", "err", err)
+			return nil
+		}
+		b.Transactions = body.Transactions
+	} else {
+		body := new(opBody)
+		if err := rlp.DecodeBytes(data, body); err != nil {
+			log.Error("Invalid block body 2 RLP", "err", err)
+			return nil
+		}
+		b.Transactions = body.Transactions
+		b.Uncles = body.Uncles
+		b.Withdrawals = body.Withdrawals
+	}
+	return nil
 }
