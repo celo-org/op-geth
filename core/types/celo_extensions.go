3290c595f20409ccaedbf0aa4b35a716f402756c
Piers Powlesland <pierspowlesland@gmail.com>
2024-03-19 14:33:22 +0000

    Ensure that header hashes match celo header hashes

--- core/types/celo_extensions.go
+++ core/types/celo_extensions.go
@@ -1,16 +1,15 @@
 package types
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"math/big"
-	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 )
-
-type IstanbulExtra rlp.RawValue
 
 type beforeGingerbreadHeader struct {
 	ParentHash  common.Hash    `json:"parentHash"       gencodec:"required"`
@@ -23,11 +22,6 @@ type beforeGingerbreadHeader struct {
 	GasUsed     uint64         `json:"gasUsed"          gencodec:"required"`
 	Time        uint64         `json:"timestamp"        gencodec:"required"`
 	Extra       []byte         `json:"extraData"        gencodec:"required"`
-
-	// Used to cache deserialized istanbul extra data
-	extraLock  sync.Mutex
-	extraValue *IstanbulExtra
-	extraError error
 }
 
 // This type is required to avoid an infinite loop when decoding
@@ -84,6 +78,49 @@ func (h *Header) DecodeRLP(s *rlp.Stream) error {
 	}
 
 	return err
+}
+
+func (h *Header) EncodeRLP(w io.Writer) error {
+	if (h.UncleHash == common.Hash{}) {
+		// Before gingerbread hardfork Celo did not include all of
+		// Ethereum's header fields. In that case we must omit the new
+		// fields from the header when encoding as RLP to maintain the same encoding and hashes.
+		// `UncleHash` is a safe way to check, since it is the zero hash before
+		// gingerbread and non-zero after.
+		rlpFields := []interface{}{
+			h.ParentHash,
+			h.Coinbase,
+			h.Root,
+			h.TxHash,
+			h.ReceiptHash,
+			h.Bloom,
+			h.Number,
+			h.GasUsed,
+			h.Time,
+			h.Extra,
+		}
+		return rlp.Encode(w, rlpFields)
+	} else {
+		rlpFields := []interface{}{
+			h.ParentHash,
+			h.UncleHash,
+			h.Coinbase,
+			h.Root,
+			h.TxHash,
+			h.ReceiptHash,
+			h.Bloom,
+			h.Difficulty,
+			h.Number,
+			h.GasLimit,
+			h.GasUsed,
+			h.Time,
+			h.Extra,
+			h.MixDigest,
+			h.Nonce,
+			h.BaseFee,
+		}
+		return rlp.Encode(w, rlpFields)
+	}
 }
 
 type CeloBody struct {
@@ -163,4 +200,76 @@ func (b *Body) DecodeRLP(s *rlp.Stream) error {
 		b.Withdrawals = body.Withdrawals
 	}
 	return nil
+}
+
+var (
+	IstanbulExtraVanity = 32 // Fixed number of extra-data bytes reserved for validator vanity
+
+	// ErrInvalidIstanbulHeaderExtra is returned if the length of extra-data is less than 32 bytes
+	ErrInvalidIstanbulHeaderExtra = errors.New("invalid istanbul header extra-data")
+)
+
+type IstanbulAggregatedSeal struct {
+	// Bitmap is a bitmap having an active bit for each validator that signed this block
+	Bitmap *big.Int
+	// Signature is an aggregated BLS signature resulting from signatures by each validator that signed this block
+	Signature []byte
+	// Round is the round in which the signature was created.
+	Round *big.Int
+}
+
+type IstanbulExtra struct {
+	// AddedValidators are the validators that have been added in the block
+	AddedValidators []common.Address
+	// AddedValidatorsPublicKeys are the BLS public keys for the validators added in the block
+	AddedValidatorsPublicKeys []rlp.RawValue
+	// RemovedValidators is a bitmap having an active bit for each removed validator in the block
+	RemovedValidators *big.Int
+	// Seal is an ECDSA signature by the proposer
+	Seal []byte
+	// AggregatedSeal contains the aggregated BLS signature created via IBFT consensus.
+	AggregatedSeal IstanbulAggregatedSeal
+	// ParentAggregatedSeal contains and aggregated BLS signature for the previous block.
+	ParentAggregatedSeal IstanbulAggregatedSeal
+}
+
+// ExtractIstanbulExtra extracts all values of the IstanbulExtra from the header. It returns an
+// error if the length of the given extra-data is less than 32 bytes or the extra-data can not
+// be decoded.
+func extractIstanbulExtra(h *Header) (*IstanbulExtra, error) {
+	if len(h.Extra) < IstanbulExtraVanity {
+		return nil, ErrInvalidIstanbulHeaderExtra
+	}
+
+	var istanbulExtra *IstanbulExtra
+	err := rlp.DecodeBytes(h.Extra[IstanbulExtraVanity:], &istanbulExtra)
+	if err != nil {
+		return nil, err
+	}
+	return istanbulExtra, nil
+}
+
+// IstanbulFilteredHeader returns a filtered header which some information (like seal, aggregated signature)
+// are clean to fulfill the Istanbul hash rules. It returns nil if the extra-data cannot be
+// decoded/encoded by rlp.
+func IstanbulFilteredHeader(h *Header, keepSeal bool) *Header {
+	newHeader := CopyHeader(h)
+	istanbulExtra, err := extractIstanbulExtra(newHeader)
+	if err != nil {
+		return nil
+	}
+
+	if !keepSeal {
+		istanbulExtra.Seal = []byte{}
+	}
+	istanbulExtra.AggregatedSeal = IstanbulAggregatedSeal{}
+
+	payload, err := rlp.EncodeToBytes(&istanbulExtra)
+	if err != nil {
+		return nil
+	}
+
+	newHeader.Extra = append(newHeader.Extra[:IstanbulExtraVanity], payload...)
+
+	return newHeader
 }
