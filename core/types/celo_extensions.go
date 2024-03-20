679dbaee07b2bdcb96a805ac6940d106e514d5fe
Piers Powlesland <pierspowlesland@gmail.com>
2024-03-20 18:32:16 +0000

    Add custom decoding for celo headers
    This allows op geth to decode old celo headers.

---
+++ core/types/celo_extensions.go
@@ -0,0 +1,88 @@
+package types
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rlp"
+	"math/big"
+	"sync"
+)
+
+type IstanbulExtra rlp.RawValue
+
+type beforeGingerbreadHeader struct {
+	ParentHash  common.Hash    `json:"parentHash"       gencodec:"required"`
+	Coinbase    common.Address `json:"miner"            gencodec:"required"`
+	Root        common.Hash    `json:"stateRoot"        gencodec:"required"`
+	TxHash      common.Hash    `json:"transactionsRoot" gencodec:"required"`
+	ReceiptHash common.Hash    `json:"receiptsRoot"     gencodec:"required"`
+	Bloom       Bloom          `json:"logsBloom"        gencodec:"required"`
+	Number      *big.Int       `json:"number"           gencodec:"required"`
+	GasUsed     uint64         `json:"gasUsed"          gencodec:"required"`
+	Time        uint64         `json:"timestamp"        gencodec:"required"`
+	Extra       []byte         `json:"extraData"        gencodec:"required"`
+
+	// Used to cache deserialized istanbul extra data
+	extraLock  sync.Mutex
+	extraValue *IstanbulExtra
+	extraError error
+}
+
+// This type is required to avoid an infinite loop when decoding
+type afterGingerbreadHeader Header
+
+func (h *Header) DecodeRLP(s *rlp.Stream) error {
+	_, size, _ := s.Kind()
+	var raw rlp.RawValue
+	err := s.Decode(&raw)
+	if err != nil {
+		return err
+	}
+	headerSize := len(raw) - int(size)
+	numElems, err := rlp.CountValues(raw[headerSize:])
+	if err != nil {
+		return err
+	}
+	if numElems == 10 {
+		// Before gingerbread
+		decodedHeader := beforeGingerbreadHeader{}
+		err = rlp.DecodeBytes(raw, &decodedHeader)
+
+		h.ParentHash = decodedHeader.ParentHash
+		h.Coinbase = decodedHeader.Coinbase
+		h.Root = decodedHeader.Root
+		h.TxHash = decodedHeader.TxHash
+		h.ReceiptHash = decodedHeader.ReceiptHash
+		h.Bloom = decodedHeader.Bloom
+		h.Number = decodedHeader.Number
+		h.GasUsed = decodedHeader.GasUsed
+		h.Time = decodedHeader.Time
+		h.Extra = decodedHeader.Extra
+	} else {
+		// After gingerbread
+		decodedHeader := afterGingerbreadHeader{}
+		err = rlp.DecodeBytes(raw, &decodedHeader)
+
+		h.ParentHash = decodedHeader.ParentHash
+		h.UncleHash = decodedHeader.UncleHash
+		h.Coinbase = decodedHeader.Coinbase
+		h.Root = decodedHeader.Root
+		h.TxHash = decodedHeader.TxHash
+		h.ReceiptHash = decodedHeader.ReceiptHash
+		h.Bloom = decodedHeader.Bloom
+		h.Difficulty = decodedHeader.Difficulty
+		h.Number = decodedHeader.Number
+		h.GasLimit = decodedHeader.GasLimit
+		h.GasUsed = decodedHeader.GasUsed
+		h.Time = decodedHeader.Time
+		h.Extra = decodedHeader.Extra
+		h.MixDigest = decodedHeader.MixDigest
+		h.Nonce = decodedHeader.Nonce
+		h.BaseFee = decodedHeader.BaseFee
+		h.WithdrawalsHash = decodedHeader.WithdrawalsHash
+		h.BlobGasUsed = decodedHeader.BlobGasUsed
+		h.ExcessBlobGas = decodedHeader.ExcessBlobGas
+		h.ParentBeaconRoot = decodedHeader.ParentBeaconRoot
+	}
+
+	return err
+}
