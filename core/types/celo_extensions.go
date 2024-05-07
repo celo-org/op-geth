d46e6fe644468479968cad121132e8998d53644d
Piers Powlesland <pierspowlesland@gmail.com>
2024-05-07 14:31:33 +0100

    Move tx type to be next to impl

--- core/types/celo_extensions.go
+++ core/types/celo_extensions.go
@@ -4,11 +4,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-const (
-	// CeloDynamicFeeTxV2Type = 0x7c  old Celo tx type with gateway fee
-	CeloDynamicFeeTxV2Type = 0x7b
-)
-
 // Returns the fee currency of the transaction if there is one.
 func (tx *Transaction) FeeCurrency() *common.Address {
 	var feeCurrency *common.Address
