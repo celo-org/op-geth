be6007b10f5af823c203cf17e973eb1ee12b5ed5
Piers Powlesland <pierspowlesland@gmail.com>
2024-05-13 18:26:35 +0100

    Remove unused function

--- rlp/celo_raw.go
+++ rlp/celo_raw.go
@@ -1,15 +1,10 @@
 package rlp
 
-// ReadKind exposes readKind so to enable introspection of rlp encoded data.
-func ReadKind(buf []byte) (k Kind, tagsize, contentsize uint64, err error) {
-	return readKind(buf)
-}
-
 // ReadNext reads the next RLP item from the given buffer and returns the remaining buffer,
 // the kind of the item, the size of the tag, the size of the content, and any error encountered.
 func ReadNext(buf []byte) (remaining []byte, kind Kind, tagsize, contentsize uint64, err error) {
 	remaining = buf
-	kind, tagsize, contentsize, err = ReadKind(remaining)
+	kind, tagsize, contentsize, err = readKind(remaining)
 	if err != nil {
 		return nil, 0, 0, 0, err
 	}
