8b31509820578aaa8256bab1b22dd14384ce2d48
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2024-05-16 16:32:59 +0200

    Add USDT and USDC to block-limit per currency defaults

--- miner/celo_defaults.go
+++ miner/celo_defaults.go
@@ -10,6 +10,8 @@ var (
 	cUSD_TOKEN  = common.HexToAddress("0x765DE816845861e75A25fCA122bb6898B8B1282a")
 	cEUR_TOKEN  = common.HexToAddress("0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73")
 	cREAL_TOKEN = common.HexToAddress("0xe8537a3d056DA446677B9E9d6c5dB704EaAb4787")
+	USDC_TOKEN  = common.HexToAddress("0xcebA9300f2b948710d2653dD7B07f33A8B32118C")
+	USDT_TOKEN  = common.HexToAddress("0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e")
 )
 
 // default limits default fraction
@@ -19,6 +21,8 @@ const DefaultFeeCurrencyLimit = 0.5
 var DefaultFeeCurrencyLimits = map[uint64]map[common.Address]float64{
 	params.CeloMainnetChainID: {
 		cUSD_TOKEN:  0.9,
+		USDT_TOKEN:  0.9,
+		USDC_TOKEN:  0.9,
 		cEUR_TOKEN:  0.5,
 		cREAL_TOKEN: 0.5,
 	},
