18236315b1d3d1df63c4fed5f257dec0cda9243c
Piers Powlesland <pierspowlesland@gmail.com>
2024-06-19 17:33:03 +0100

    Add more no cel2 config

--- eth/downloader/testchain_test.go
+++ eth/downloader/testchain_test.go
@@ -40,7 +40,7 @@ var (
 	testDB      = rawdb.NewMemoryDatabase()
 
 	testGspec = &core.Genesis{
-		Config:  params.TestChainConfig,
+		Config:  params.TestChainConfigNoCel2,
 		Alloc:   types.GenesisAlloc{testAddress: {Balance: big.NewInt(1000000000000000)}},
 		BaseFee: big.NewInt(params.InitialBaseFee),
 	}
