33d771fad5cb4bde6ec8a177048175688dd7c35b
Javier Cortejoso <javier.cortejoso@gmail.com>
2024-09-23 19:24:59 +0200

    blockchain: Added new metrics: chain_txincluded, chain_basefee and chain_gasused (#224)
    * Added metrics chain_txincluded, chain_basefee and chain_gasused
    
    * Safe-check for nil values

--- core/blockchain.go
+++ core/blockchain.go
@@ -97,6 +97,9 @@ var (
 	blockCrossValidationTimer = metrics.NewRegisteredResettingTimer("chain/crossvalidation", nil)
 	blockExecutionTimer       = metrics.NewRegisteredResettingTimer("chain/execution", nil)
 	blockWriteTimer           = metrics.NewRegisteredResettingTimer("chain/write", nil)
+	blockGasUsedGauge         = metrics.NewRegisteredGauge("chain/gasused", nil)
+	blockBaseFeeGauge         = metrics.NewRegisteredGauge("chain/basefee", nil)
+	blockTxIncluded           = metrics.NewRegisteredGauge("chain/txincluded", nil)
 
 	blockReorgMeter     = metrics.NewRegisteredMeter("chain/reorg/executes", nil)
 	blockReorgAddMeter  = metrics.NewRegisteredMeter("chain/reorg/add", nil)
@@ -2181,6 +2184,14 @@ func (bc *BlockChain) ProcessBlock(parentRoot common.Hash, block *types.Block, s
 	blockWriteTimer.Update(time.Since(wstart) - max(statedb.AccountCommits, statedb.StorageCommits) /* concurrent */ - statedb.SnapshotCommits - statedb.TrieDBCommits)
 	elapsed := time.Since(startTime) + 1 // prevent zero division
 	blockInsertTimer.Update(elapsed)
+
+	blockGasUsedGauge.Update(int64(res.GasUsed))
+	if block.Header().BaseFee != nil {
+		blockBaseFeeGauge.Update(block.Header().BaseFee.Int64())
+	}
+	if block.Transactions() != nil {
+		blockTxIncluded.Update(int64(block.Transactions().Len()))
+	}
 
 	// TODO(rjl493456442) generalize the ResettingTimer
 	mgasps := float64(res.GasUsed) * 1000 / float64(elapsed)
