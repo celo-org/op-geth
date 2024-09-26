03b466f62925809776494ddb4601bebaf4f2ded1
Paul Lange <palango@gmx.de>
2024-09-26 20:18:43 +0200

    Fix fee currency usage in newTxWithMinerFee
    Tip and base fee handling was not aware of fee currencies in that
    function, causing wrong tx handling inside the tx pool.

--- miner/ordering_test.go
+++ miner/ordering_test.go
@@ -102,7 +102,7 @@ func testTransactionPriceNonceSort(t *testing.T, baseFee *big.Int) {
 		expectedCount += count
 	}
 	// Sort the transactions and cross check the nonce ordering
-	txset := newTransactionsByPriceAndNonce(signer, groups, baseFee)
+	txset := newTransactionsByPriceAndNonce(signer, groups, baseFee, nil)
 
 	txs := types.Transactions{}
 	for tx, _ := txset.Peek(); tx != nil; tx, _ = txset.Peek() {
@@ -168,7 +168,7 @@ func TestTransactionTimeSort(t *testing.T) {
 		})
 	}
 	// Sort the transactions and cross check the nonce ordering
-	txset := newTransactionsByPriceAndNonce(signer, groups, nil)
+	txset := newTransactionsByPriceAndNonce(signer, groups, nil, nil)
 
 	txs := types.Transactions{}
 	for tx, _ := txset.Peek(); tx != nil; tx, _ = txset.Peek() {
@@ -192,5 +192,57 @@ func TestTransactionTimeSort(t *testing.T) {
 				t.Errorf("invalid received time ordering: tx #%d (A=%x T=%v) > tx #%d (A=%x T=%v)", i, fromi[:4], txi.Time(), i+1, fromNext[:4], next.Time())
 			}
 		}
+	}
+}
+
+func TestCorrectTransactionSortWithFeeCurrency(t *testing.T) {
+	t.Parallel()
+
+	user1, _ := crypto.GenerateKey()
+	user2, _ := crypto.GenerateKey()
+	signer := types.LatestSignerForChainID(big.NewInt(1))
+
+	rates := make(common.ExchangeRates)
+	feeCurrency := common.BigToAddress(common.Big1)
+	rates[feeCurrency] = big.NewRat(10, 1)
+
+	groups := map[common.Address][]*txpool.LazyTransaction{}
+
+	baseFee := big.NewInt(10)
+
+	addr1 := crypto.PubkeyToAddress(user1.PublicKey)
+	user1GasFeeCap := uint256.NewInt(950)
+	// user1 pays 950 feeCurrency, rate is 10/1, baseFee is 10 in feeCurrency is 100,
+	// 950 - 100 = 850 feeCurrency tip => 85 celo tip
+	// without converting the tip (error fixed): 950 - 10 = 940 feeCurrency tip => 94 celo tip (this test fails)
+	groups[addr1] = append(groups[addr1], &txpool.LazyTransaction{
+		GasFeeCap:   user1GasFeeCap,
+		GasTipCap:   user1GasFeeCap,
+		Gas:         100,
+		FeeCurrency: &feeCurrency,
+	})
+
+	addr2 := crypto.PubkeyToAddress(user2.PublicKey)
+	user2GasFeeCap := uint256.NewInt(100)
+	// user2 pays 100 celos, baseFee is 10, tip is 90
+	groups[addr2] = append(groups[addr2], &txpool.LazyTransaction{
+		GasFeeCap: user2GasFeeCap,
+		GasTipCap: user2GasFeeCap,
+		Gas:       100,
+	})
+
+	// Sort the transactions and cross check the nonce ordering
+	txset := newTransactionsByPriceAndNonce(signer, groups, baseFee, rates)
+
+	var auxTx *txpool.LazyTransaction
+	var fees *uint256.Int
+	auxTx, fees = txset.Peek()
+	if auxTx.FeeCurrency != nil || auxTx.GasFeeCap.Cmp(user2GasFeeCap) != 0 || fees.Cmp(uint256.NewInt(90)) != 0 {
+		t.Error("expected tx from user2, got the tx from user1")
+	}
+	txset.Shift()
+	auxTx, fees = txset.Peek()
+	if auxTx.FeeCurrency == nil || *auxTx.FeeCurrency != feeCurrency || auxTx.GasFeeCap.Cmp(user1GasFeeCap) != 0 || fees.Cmp(uint256.NewInt(85)) != 0 {
+		t.Error("expected tx from user1, got the tx from user2")
 	}
 }
