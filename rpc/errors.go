fd593f27afa2975b439acdb63bb270a58d1438ad
piersy <pierspowlesland@gmail.com>
2024-10-03 18:36:30 +0100

    Revert historical rpc (#247)
    This reverts the changes made to trigger the historical RPC based on the cel2
    block time. (although we retain support for historical execution for
    debug_traceCall)
    
    The reason for this is that there are [tests] in optimism
    (https://github.com/celo-org/optimism/blob/78d41a3be3c1759e32965a9eb2e69c146eda02b6/op-e2e/op_geth_test.go#L232)
    that test pre and post time based forks. These test were failing on this PR:
    
    * https://github.com/celo-org/optimism/pull/243
    
    The function used to [construct the
    genesis](https://github.com/celo-org/optimism/blob/78d41a3be3c1759e32965a9eb2e69c146eda02b6/op-chain-ops/genesis/genesis.go#L25)
    assumes that the time based forks take the time of the L1 start block and sets
    all time based forks to the L1 start block time. If we set the Cel2Time to the
    L1 start block time and then try to test some pre fork execution behaviour, the
    op-geth RPC api will interpret that request as a request that needs to be
    proxied but in these tests no proxy is set so the tests fail.
    
    Previously the test relied on the fact that the bedrock block defaults to zero,
    and since the old RPC implementation only proxies requests older than bedrock
    it ensures that no requests need proxying.
    
    It turns out that we can use the bedrock block as well, since we always set the
    bedrock block to be the same block as the migration block. So simply removing
    the changes made for historical RPC actually works well, and fixes the tests in
    the aforementioned PR.

--- rpc/errors.go
+++ rpc/errors.go
@@ -80,7 +80,7 @@ type NoHistoricalFallbackError struct{}
 func (e NoHistoricalFallbackError) ErrorCode() int { return -32801 }
 
 func (e NoHistoricalFallbackError) Error() string {
-	return "no historical RPC is available for this historical (pre-cel2) execution request"
+	return "no historical RPC is available for this historical (pre-bedrock) execution request"
 }
 
 type methodNotFoundError struct{ method string }
