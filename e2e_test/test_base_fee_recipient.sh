96ea072798baf116e34345d760d0a9f5140dea02
Piers Powlesland <pierspowlesland@gmail.com>
2024-10-09 19:15:05 +0100

    Ensure that base fee test still works on devnet

--- e2e_test/test_base_fee_recipient.sh
+++ e2e_test/test_base_fee_recipient.sh
@@ -10,15 +10,17 @@ tx_json=$(cast send --json --private-key $ACC_PRIVKEY $TOKEN_ADDR 'transfer(addr
 block_number=$(echo $tx_json | jq -r '.blockNumber' | cast to-dec)
 block=$(cast block --json --full $block_number)
 gas_used=$(echo $block | jq -r '.gasUsed' | cast to-dec)
-# Every block contains a system tx that pays nothing for gas so we must subtract this from the total gas per block
-system_tx_hash=$(echo $block | jq -r '.transactions[] | select(.from == "0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001") | .hash')
-system_tx_gas=$(cast receipt --json $system_tx_hash | jq -r '.cumulativeGasUsed' | cast to-dec)
+base_fee=$(echo $block | jq -r '.baseFeePerGas' | cast to-dec)
+if [[ -n $NETWORK  ]]; then
+	# Every block in a non dev system contains a system tx that pays nothing for gas so we must subtract this from the total gas per block
+	system_tx_hash=$(echo $block | jq -r '.transactions[] | select(.from == "0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001") | .hash')
+	system_tx_gas=$(cast receipt --json $system_tx_hash | jq -r '.cumulativeGasUsed' | cast to-dec)
+	gas_used=$((gas_used - system_tx_gas))
+fi
 balance_before=$(cast balance --block $((block_number-1)) $FEE_HANDLER)
 balance_after=$(cast balance --block $block_number $FEE_HANDLER)
 balance_change=$((balance_after - balance_before))
-base_fee=$(echo $block | jq -r '.baseFeePerGas' | cast to-dec)
-
-expected_balance_change=$((base_fee * (gas_used-system_tx_gas)))
+expected_balance_change=$((base_fee * gas_used))
 [[ $expected_balance_change -eq $balance_change ]] || (
   echo "Balance did not change as expected"
   exit 1
