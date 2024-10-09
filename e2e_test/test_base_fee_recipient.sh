315163ccc8981f019d8de9a0a72dbf930066de51
Piers Powlesland <pierspowlesland@gmail.com>
2024-10-09 14:29:07 +0100

    Update base fee receipient test
    This test assumed that it was running in an isolated environment when
    calculating the balance increase of the base fee recipient.
    
    Now that it can run on a live network we need to adjust the calculation
    to account for transactions sent by third parties.
    
    Now the test submits a transaction and calculates the expected balance
    increase of the base fee receipient using the block that the transaction
    was mined in.

--- e2e_test/test_base_fee_recipient.sh
+++ e2e_test/test_base_fee_recipient.sh
@@ -6,15 +6,20 @@ set -x
 source shared.sh
 
 # Send token and check balance
-balance_before=$(cast balance $FEE_HANDLER)
 tx_json=$(cast send --json --private-key $ACC_PRIVKEY $TOKEN_ADDR 'transfer(address to, uint256 value) returns (bool)' 0x000000000000000000000000000000000000dEaD 100)
-gas_used=$(echo $tx_json | jq -r '.gasUsed')
 block_number=$(echo $tx_json | jq -r '.blockNumber' | cast to-dec)
-base_fee=$(cast base-fee $block_number)
-expected_balance_change=$((base_fee * gas_used))
-balance_after=$(cast balance $FEE_HANDLER)
-echo "Balance change: $balance_before -> $balance_after"
-[[ $((balance_before + expected_balance_change)) -eq $balance_after ]] || (
+block=$(cast block --json --full $block_number)
+gas_used=$(echo $block | jq -r '.gasUsed' | cast to-dec)
+# Every block contains a system tx that pays nothing for gas so we must subtract this from the total gas per block
+system_tx_hash=$(echo $block | jq -r '.transactions[] | select(.from == "0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001") | .hash')
+system_tx_gas=$(cast receipt --json $system_tx_hash | jq -r '.cumulativeGasUsed' | cast to-dec)
+balance_before=$(cast balance --block $((block_number-1)) $FEE_HANDLER)
+balance_after=$(cast balance --block $block_number $FEE_HANDLER)
+balance_change=$((balance_after - balance_before))
+base_fee=$(echo $block | jq -r '.baseFeePerGas' | cast to-dec)
+
+expected_balance_change=$((base_fee * (gas_used-system_tx_gas)))
+[[ $expected_balance_change -eq $balance_change ]] || (
   echo "Balance did not change as expected"
   exit 1
 )
