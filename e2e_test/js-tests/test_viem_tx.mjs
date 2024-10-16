4dca89471ae2a1a7001b7aba45d9fb267b49543b
Piers Powlesland <pierspowlesland@gmail.com>
2024-10-16 21:41:12 +0100

    Update tests to work on alfajores

--- e2e_test/js-tests/test_viem_tx.mjs
+++ e2e_test/js-tests/test_viem_tx.mjs
@@ -5,6 +5,7 @@ import {
 	createWalletClient,
 	http,
 	defineChain,
+	parseAbi,
 } from "viem";
 import { celoAlfajores } from "viem/chains";
 import { privateKeyToAccount } from "viem/accounts";
@@ -43,6 +44,12 @@ const walletClient = createWalletClient({
 	transport: http(),
 });
 
+// Returns the base fee per gas for the current block multiplied by 2 to account for any increase in the subsequent block.
+async function getGasFees(publicClient, tip) {
+	const b = await publicClient.getBlock();
+	return [BigInt(b.baseFeePerGas) * 2n + tip, tip];
+}
+
 const testNonceBump = async (
 	firstCap,
 	firstCurrency,
@@ -60,7 +67,7 @@ const testNonceBump = async (
 		account,
 		to: "0x00000000000000000000000000000000DeaDBeef",
 		value: 2,
-		gas: 90000,
+		gas: 171000,
 		maxFeePerGas: firstCap,
 		maxPriorityFeePerGas: firstCap,
 		nonce: syncBarrierRequest.nonce + 1,
@@ -72,7 +79,8 @@ const testNonceBump = async (
 			account,
 			to: "0x00000000000000000000000000000000DeaDBeef",
 			value: 3,
-			gas: 90000,
+			// dynaimcally retrieve intrinsic gas from feeCurrency directory "feeCurrencyConfig"
+			gas: 171000,
 			maxFeePerGas: secondCap,
 			maxPriorityFeePerGas: secondCap,
 			nonce: syncBarrierRequest.nonce + 1,
@@ -86,7 +94,7 @@ const testNonceBump = async (
 			shouldReplace
 		) {
 			throw err; // Only throw if unexpected error.
-		}
+	  }
 	}
 	const syncBarrierSignature =
 		await walletClient.signTransaction(syncBarrierRequest);
@@ -119,7 +127,7 @@ describe("viem send tx", () => {
 		assert.equal(receipt.status, "success", "receipt status 'failure'");
 	}).timeout(10_000);
 
-	it("send tx with gas estimation and check receipt", async () => {
+	it("send basic tx using viem gas estimation and check receipt", async () => {
 		const request = await walletClient.prepareTransactionRequest({
 			account,
 			to: "0x00000000000000000000000000000000DeaDBeef",
@@ -133,15 +141,16 @@ describe("viem send tx", () => {
 		assert.equal(receipt.status, "success", "receipt status 'failure'");
 	}).timeout(10_000);
 
-	it("send fee currency tx and check receipt", async () => {
+	it("send fee currency tx with explicit gas fields and check receipt", async () => {
+		const [maxFeePerGas, tip] = await getGasFees(publicClient, 2n);
 		const request = await walletClient.prepareTransactionRequest({
 			account,
 			to: "0x00000000000000000000000000000000DeaDBeef",
 			value: 2,
-			gas: 90000,
+			gas: 171000,
 			feeCurrency: process.env.FEE_CURRENCY,
-			maxFeePerGas: 2000000000n,
-			maxPriorityFeePerGas: 2n,
+			maxFeePerGas: maxFeePerGas,
+			maxPriorityFeePerGas: tip,
 		});
 		const signature = await walletClient.signTransaction(request);
 		const hash = await walletClient.sendRawTransaction({
@@ -156,33 +165,51 @@ describe("viem send tx", () => {
 			account,
 			to: "0x00000000000000000000000000000000DeaDBeef",
 			value: 2,
-			gas: 90000,
+			gas: 171000,
 			feeCurrency: process.env.FEE_CURRENCY,
 		});
 
+		// Get the raw gas price and maxPriorityFeePerGas
 		const gasPriceNative = await publicClient.getGasPrice({});
 		var maxPriorityFeePerGasNative =
 			await publicClient.estimateMaxPriorityFeePerGas({});
 		const block = await publicClient.getBlock({});
+
+		// Check them against the base fee.
 		assert.equal(
 			BigInt(block.baseFeePerGas) + maxPriorityFeePerGasNative,
 			gasPriceNative,
 		);
 
-		// viem's getGasPrice does not expose additional request parameters,
-		// but Celo's override 'chain.fees.estimateFeesPerGas' action does.
-		// this will call the eth_gasPrice and eth_maxPriorityFeePerGas methods
-		// with the additional feeCurrency parameter internally
+		// viem's getGasPrice does not expose additional request parameters, but
+		// Celo's override 'chain.fees.estimateFeesPerGas' action does. This will
+		// call the eth_gasPrice and eth_maxPriorityFeePerGas methods with the
+		// additional feeCurrency parameter internally, it also multiplies the base
+		// fee component of the maxFeePerGas by a multiplier which by default is
+		// 1.2 or (12n/10n).
 		var fees = await publicClient.estimateFeesPerGas({
 			type: "eip1559",
 			request: {
 				feeCurrency: process.env.FEE_CURRENCY,
 			},
 		});
-		// first check that the fee currency denominated gas price
-		// converts properly to the native gas price
-		assert.equal(fees.maxFeePerGas, gasPriceNative * 2n);
-		assert.equal(fees.maxPriorityFeePerGas, maxPriorityFeePerGasNative * 2n);
+
+		// Get the exchange rates for the fee currency.
+		const abi = parseAbi(['function getExchangeRate(address token) public view returns (uint256 numerator, uint256 denominator)']);
+		const [numerator, denominator] = await publicClient.readContract({
+			address: process.env.FEE_CURRENCY_DIRECTORY_ADDR,
+			abi: abi,
+			functionName: 'getExchangeRate',
+			args: [process.env.FEE_CURRENCY],
+		});
+
+		// TODO fix this when viem is fixed - https://github.com/celo-org/viem/pull/20
+		// The expected value for the max fee should be the (baseFeePerGas * multiplier) + maxPriorityFeePerGas
+		// Instead what is currently returned is (maxFeePerGas * multiplier) + maxPriorityFeePerGas
+		const maxPriorityFeeInFeeCurrency = (maxPriorityFeePerGasNative * numerator) / denominator;
+    const maxFeeInFeeCurrency = ((block.baseFeePerGas +maxPriorityFeePerGasNative)*numerator) /denominator;
+		assert.equal(fees.maxFeePerGas, ((maxFeeInFeeCurrency*12n)/10n) + maxPriorityFeeInFeeCurrency);
+		assert.equal(fees.maxPriorityFeePerGas, maxPriorityFeeInFeeCurrency);
 
 		// check that the prepared transaction request uses the
 		// converted gas price internally
@@ -196,7 +223,7 @@ describe("viem send tx", () => {
 			to: "0x00000000000000000000000000000000DeaDBeef",
 			value: 2,
 			feeCurrency: process.env.FEE_CURRENCY,
-			maxFeePerGas: 2000000000n,
+			maxFeePerGas: 50000000000n,
 			maxPriorityFeePerGas: 2n,
 		});
 		const signature = await walletClient.signTransaction(request);
@@ -209,19 +236,14 @@ describe("viem send tx", () => {
 
 	it("send overlapping nonce tx in different currencies", async () => {
 		const priceBump = 1.1;
-		const rate = 2;
+
+		const rate = await getRate(process.env.FEE_CURRENCY);
 		// Native to FEE_CURRENCY
 		const nativeCap = 30_000_000_000;
-		const bumpCurrencyCap = BigInt(Math.round(nativeCap * rate * priceBump));
-		const failToBumpCurrencyCap = BigInt(
-			Math.round(nativeCap * rate * priceBump) - 1,
-		);
-		// FEE_CURRENCY to Native
-		const currencyCap = 60_000_000_000;
-		const bumpNativeCap = BigInt(Math.round((currencyCap * priceBump) / rate));
-		const failToBumpNativeCap = BigInt(
-			Math.round((currencyCap * priceBump) / rate) - 1,
-		);
+		const bumpCurrencyCap = rate.toFeeCurrency(BigInt(Math.round(nativeCap * priceBump)));
+		const failToBumpCurrencyCap = rate.toFeeCurrency(BigInt(
+			Math.round(nativeCap * priceBump) - 1,
+		));
 		const tokenCurrency = process.env.FEE_CURRENCY;
 		const nativeCurrency = null;
 		await testNonceBump(
@@ -238,6 +260,13 @@ describe("viem send tx", () => {
 			tokenCurrency,
 			false,
 		);
+
+		// FEE_CURRENCY to Native
+		const currencyCap = 60_000_000_000;
+		const bumpNativeCap = rate.toNative(BigInt(Math.round(currencyCap * priceBump)));
+		const failToBumpNativeCap = rate.toNative(BigInt(
+			Math.round(currencyCap * priceBump) - 1,
+		));
 		await testNonceBump(
 			currencyCap,
 			tokenCurrency,
@@ -252,14 +281,14 @@ describe("viem send tx", () => {
 			nativeCurrency,
 			false,
 		);
-	}).timeout(20_000);
+	}).timeout(40_000);
 
 	it("send tx with unregistered fee currency", async () => {
 		const request = await walletClient.prepareTransactionRequest({
 			account,
 			to: "0x00000000000000000000000000000000DeaDBeef",
 			value: 2,
-			gas: 90000,
+			gas: 171000,
 			feeCurrency: "0x000000000000000000000000000000000badc310",
 			maxFeePerGas: 1000000000n,
 			maxPriorityFeePerGas: 1n,
@@ -281,18 +310,19 @@ describe("viem send tx", () => {
 	}).timeout(10_000);
 
 	it("send fee currency tx with just high enough gas price", async () => {
+		const rate = await getRate(process.env.FEE_CURRENCY);
 		const block = await publicClient.getBlock({});
 		// Actually, the base fee will be a bit lower next block, since our blocks
 		// are always mostly empty. But the difference will be much less than the
 		// exchange rate of 2, so the test will still check that we take the fee
 		// currency into account everywhere.
-		const maxFeePerGas = block.baseFeePerGas / 2n;
+		const maxFeePerGas = rate.toFeeCurrency(block.baseFeePerGas);
 		const request = await walletClient.prepareTransactionRequest({
 			account,
 			to: "0x00000000000000000000000000000000DeaDBeef",
 			value: 2,
-			gas: 90000,
-			feeCurrency: process.env.FEE_CURRENCY2,
+			gas: 171000,
+			feeCurrency: process.env.FEE_CURRENCY,
 			maxFeePerGas,
 			maxPriorityFeePerGas: 1n,
 		});
@@ -302,5 +332,19 @@ describe("viem send tx", () => {
 		});
 		const receipt = await publicClient.waitForTransactionReceipt({ hash });
 		assert.equal(receipt.status, "success", "receipt status 'failure'");
-	}).timeout(5_000);
+	}).timeout(10_000);
 });
+
+async function getRate(feeCurrencyAddress) {
+		const abi = parseAbi(['function getExchangeRate(address token) public view returns (uint256 numerator, uint256 denominator)']);
+		const [numerator, denominator] = await publicClient.readContract({
+			address: process.env.FEE_CURRENCY_DIRECTORY_ADDR,
+			abi: abi,
+			functionName: 'getExchangeRate',
+			args: [process.env.FEE_CURRENCY],
+		});
+	return {
+		toFeeCurrency: (v) => (v * numerator) / denominator,
+		toNative: (v) => (v * denominator) / numerator,
+	};
+}
