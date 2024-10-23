bba72f797902ecd4adcdc2d359489a447b27e9fa
Piers Powlesland <pierspowlesland@gmail.com>
2024-10-23 15:57:18 +0100

    Move unsupported tx tests from viem to go
    Due to the way that viem is structured it is a bad idea for us to block
    transactions based on the supplied tx attributes, since they may be supported by
    downstream components such as optimism's custom viem functionality.
    
    So rather than trying to test unsupported txs via viem, we now test via
    golang which allows us full control of the sent transaction, thereby
    ensuring that rejection is happening at the node level.
    
    This commit also extends the error message for deprecated transactions
    to distinguish between celo legacy and plain legacy transactions.

--- e2e_test/test_smoketest.sh
+++ e2e_test/test_smoketest.sh
@@ -6,4 +6,6 @@ prepare_node
 
 (cd debug-fee-currency && forge build)
 export COMPILED_TEST_CONTRACT=../debug-fee-currency/out/DebugFeeCurrency.sol/DebugFeeCurrency.json
-cd js-tests && ./node_modules/mocha/bin/mocha.js test_viem_smoketest.mjs --timeout 25000 --exit
+(cd js-tests && ./node_modules/mocha/bin/mocha.js test_viem_smoketest.mjs --timeout 25000 --exit)
+echo  go test -v ./smoketest_unsupported -op-geth-url $ETH_RPC_URL -private-key $ACC_PRIVKEY -fee-currency $FEE_CURRENCY
+go test -v ./smoketest_unsupported_txs -op-geth-url $ETH_RPC_URL -private-key $ACC_PRIVKEY -fee-currency $FEE_CURRENCY
