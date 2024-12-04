e0c4758b09f3286648057f7d7d33beda35bb0a4c
kourin <yoshiki.takabayashi@gmail.com>
2024-12-04 20:07:47 +0900

    Replace Header RLP encoding/decoding with generated code (#288)
    Fixes https://github.com/celo-org/celo-blockchain-planning/issues/720
    
    This PR replaces the current RLP encoding/decoding of 'BeforeGingerbreadHeader' and 'AfterGingerbreadHeader' from reflection style function to auto generated function by `rlpgen`.
    
    For code generation, `BeforeGingerbreadHeader` and `AfterGingerbreadHeader` are renamed from `beforeGingerbreadHeader` and `afterGingerbreadHeader`
    
    ---------
    
    Co-authored-by: piersy <pierspowlesland@gmail.com>

--- core/types/block.go
+++ core/types/block.go
@@ -68,7 +68,6 @@ type ExecutionWitness struct {
 }
 
 //go:generate go run github.com/fjl/gencodec -type Header -field-override headerMarshaling -out gen_header_json.go
-//go:generate go run ../../rlp/rlpgen -type Header -out gen_header_rlp.go
 
 // Header represents a block header in the Ethereum blockchain.
 type Header struct {
