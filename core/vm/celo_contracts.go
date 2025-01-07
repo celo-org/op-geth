85cf9ca17170f2f9c733016b059d6bc9a0814c26
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2025-01-07 14:04:42 +0100

    Add more verbosity to contract-address getter defaults (#305)
    Before the `GetAddresses` function silently returned the
    mainnet addresses as a default value, without the caller
    being made aware of this.
    
    Now, the `GetAddresses` function will return `nil` whenever
    it does not explicitly maps the passed in chain-id to a set
    of addresses.
    
    The newly added `GetAddressesOrDefault` function allows for
    retrieving the addresses with a default fallback for
    easier use in inline calls. This replicates the behavior
    of the old `GetAddresses` functionality with more verbosity.

--- core/vm/celo_contracts.go
+++ core/vm/celo_contracts.go
@@ -47,7 +47,7 @@ func celoPrecompileAddress(index byte) common.Address {
 }
 
 func (ctx *celoPrecompileContext) IsCallerCeloToken() (bool, error) {
-	tokenAddress := addresses.GetAddresses(ctx.evm.ChainConfig().ChainID).CeloToken
+	tokenAddress := addresses.GetAddressesOrDefault(ctx.evm.ChainConfig().ChainID, addresses.MainnetAddresses).CeloToken
 
 	return tokenAddress == ctx.caller, nil
 }
