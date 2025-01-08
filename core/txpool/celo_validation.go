4b9f02850451f4f945778fc6077024987190b40c
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2025-01-08 11:32:41 +0100

    txpool: Invalidate txs with below base-fee-floor gas-price in txpool  (#292, #367)
    Fixes #291
    Fixes #304
    
    Before, the transaction pool accepted transactions that have a
    gas-price below the configured base-fee-floor, which would never be
    executed.
    
    Now, the transaction pool rejects those transactions immediately -
    giving the submitting user an immediate response and better UX.
    
    Ensure we log the convereted tip, and include the fee currency as well.
    
    Co-authored-by: piersy <pierspowlesland@gmail.com>

--- core/txpool/celo_validation.go
+++ core/txpool/celo_validation.go
@@ -1,12 +1,21 @@
 package txpool
 
 import (
+	"errors"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/exchange"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
+)
+
+var (
+
+	// ErrGasPriceDoesNotExceedBaseFeeFloor is returned if the gas price specified is
+	// lower than the configured base-fee-floor
+	ErrGasPriceDoesNotExceedBaseFeeFloor = errors.New("gas-price is less than the base-fee-floor")
+	ErrMinimumEffectiveGasTipBelowMinTip = errors.New("effective gas tip at base-fee-floor is below threshold")
 )
 
 // AcceptSet is a set of accepted transaction types for a transaction subpool.
@@ -53,9 +62,35 @@ func CeloValidateTransaction(tx *types.Transaction, head *types.Header,
 	if err := ValidateTransaction(tx, head, signer, opts, currencyCtx); err != nil {
 		return err
 	}
+
 	if !common.IsCurrencyAllowed(currencyCtx.ExchangeRates, tx.FeeCurrency()) {
 		return exchange.ErrUnregisteredFeeCurrency
 	}
 
+	if opts.Config.Celo != nil {
+		baseFeeFloor, err := exchange.ConvertCeloToCurrency(currencyCtx.ExchangeRates, tx.FeeCurrency(), new(big.Int).SetUint64(opts.Config.Celo.EIP1559BaseFeeFloor))
+		if err != nil {
+			return err
+		}
+
+		// Check that the fee cap exceeds the base fee floor
+		if baseFeeFloor.Cmp(tx.GasFeeCap()) == 1 {
+			return ErrGasPriceDoesNotExceedBaseFeeFloor
+		}
+
+		// Make sure that the effective gas tip at the base fee floor is at least the
+		// requested min-tip.
+		// The min-tip for local transactions is set to 0, we can skip checking here.
+		if opts.MinTip != nil && opts.MinTip.Cmp(new(big.Int)) > 0 {
+			// If not, this would never be included, so we can reject early.
+			minTip, err := exchange.ConvertCeloToCurrency(currencyCtx.ExchangeRates, tx.FeeCurrency(), opts.MinTip)
+			if err != nil {
+				return err
+			}
+			if tx.EffectiveGasTipIntCmp(minTip, baseFeeFloor) < 0 {
+				return ErrUnderpriced
+			}
+		}
+	}
 	return nil
 }
