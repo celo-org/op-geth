f7c5a7a15fa6b1074c7699ddfcfd6c8c6fda166e
kourin <kourin.code@gmail.com>
2025-01-15 16:43:43 +0900

    rpc: Supplement baseFee and gasLimit for pre gingerbread blocks (#302)
    * [WIP] Retrieve baseFee and gasLimit for pre gingerbread block in RPC request
    
    * Add codes to store pre gingerbread block's base fee and gas limit into KVS
    
    * Add tests
    
    * Format code
    
    * Format code
    
    * Fix test
    
    * Fix test
    
    * Add retrievePreGingerbreadGasLimit to resolve broken tests
    
    * Add test comments
    
    * Add tests for PopulatePreGingerbreadHeaderFields and retrievePreGingerbreadBlockBaseFee"
    
    * Fix lint error
    
    * Fix lint error
    
    * Remove gas limit field from pre-Gingerbread additional fields record in local DB
    
    * Replace some assert with require in tests
    
    * Format codes based on review
    
    * Clean up codes
    
    * Remove unnecessary new line
    
    * Fix typo

--- params/celo_config.go
+++ params/celo_config.go
@@ -9,7 +9,48 @@ const (
 	CeloSepoliaChainID = 11142220
 )
 
+// GasLimits holds the gas limit changes for a given chain
+type GasLimits struct {
+	changes []LimitChange
+}
+
+type LimitChange struct {
+	block    *big.Int
+	gasLimit uint64
+}
+
+// Limit returns the gas limit at a given block number
+func (g *GasLimits) Limit(block *big.Int) uint64 {
+	// Grab the gas limit at block 0
+	curr := g.changes[0].gasLimit
+	for _, c := range g.changes[1:] {
+		if block.Cmp(c.block) < 0 {
+			return curr
+		}
+		curr = c.gasLimit
+	}
+	return curr
+}
+
 var (
+	// Hardcoded set of gas limit changes derived from historical state of Celo L1 chain
+	// Ported from celo-blockchain (https://github.com/celo-org/celo-blockchain/blob/master/params/config.go#L189-L220)
+	mainnetGasLimits = &GasLimits{
+		changes: []LimitChange{
+			{big.NewInt(0), 20e6},
+			{big.NewInt(3317), 10e6},
+			{big.NewInt(3251772), 13e6},
+			{big.NewInt(6137285), 20e6},
+			{big.NewInt(13562578), 50e6},
+			{big.NewInt(14137511), 20e6},
+			{big.NewInt(21355415), 32e6},
+		},
+	}
+
+	PreGingerbreadNetworkGasLimits = map[uint64]*GasLimits{
+		CeloMainnetChainID: mainnetGasLimits,
+	}
+
 	// This config should be kept up to date with our mainnet config so that the --dev flag produces
 	// results as close as possible to mainnet.
 	DevChainConfig = &ChainConfig{
