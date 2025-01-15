db006d0c0c4e7d4257e06dcd92fda09ab248c19f
kourin <kourin.code@gmail.com>
2025-01-15 16:43:43 +0900

    Supplement baseFee and gasLimit for pre gingerbread blocks (#302)
    * [WIP] Retrieve baseFee and gasLimit for pre gingerbread block in RPC request
    
    * Add codes to store pre gingerbread block's base fee and gas limit into KVS
    
    * Add tests
    
    * Format code
    
    * Format code
    
    * Fix test
    
    * Fix test
    
    * Add retrievePreGingerbreadGasLimit to resolve broken tests
    
    * Add test comments
    
    * Add tests for PopulatePreGingerbreadHeaderFields and retrievePreGingerbreadBlockBaseFee"
    
    * Fix lint error
    
    * Fix lint error
    
    * Remove gas limit field from pre-Gingerbread additional fields record in local DB
    
    * Replace some assert with require in tests
    
    * Format codes based on review
    
    * Clean up codes
    
    * Remove unnecessary new line
    
    * Fix typo

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -843,6 +843,7 @@ func decodeHash(s string) (h common.Hash, inputLength int, err error) {
 func (api *BlockChainAPI) GetHeaderByNumber(ctx context.Context, number rpc.BlockNumber) (map[string]interface{}, error) {
 	header, err := api.b.HeaderByNumber(ctx, number)
 	if header != nil && err == nil {
+		header := PopulatePreGingerbreadHeaderFields(ctx, api.b, header)
 		response := RPCMarshalHeader(header)
 		if number == rpc.PendingBlockNumber && api.b.ChainConfig().Optimism == nil {
 			// Pending header need to nil out a few fields
@@ -859,6 +860,7 @@ func (api *BlockChainAPI) GetHeaderByNumber(ctx context.Context, number rpc.Bloc
 func (api *BlockChainAPI) GetHeaderByHash(ctx context.Context, hash common.Hash) map[string]interface{} {
 	header, _ := api.b.HeaderByHash(ctx, hash)
 	if header != nil {
+		header := PopulatePreGingerbreadHeaderFields(ctx, api.b, header)
 		return RPCMarshalHeader(header)
 	}
 	return nil
@@ -874,6 +876,7 @@ func (api *BlockChainAPI) GetHeaderByHash(ctx context.Context, hash common.Hash)
 func (api *BlockChainAPI) GetBlockByNumber(ctx context.Context, number rpc.BlockNumber, fullTx bool) (map[string]interface{}, error) {
 	block, err := api.b.BlockByNumber(ctx, number)
 	if block != nil && err == nil {
+		block := PopulatePreGingerbreadBlockFields(ctx, api.b, block)
 		response, err := RPCMarshalBlock(ctx, block, true, fullTx, api.b.ChainConfig(), api.b)
 		if err == nil && number == rpc.PendingBlockNumber && api.b.ChainConfig().Optimism == nil {
 			// Pending blocks need to nil out a few fields
@@ -891,6 +894,7 @@ func (api *BlockChainAPI) GetBlockByNumber(ctx context.Context, number rpc.Block
 func (api *BlockChainAPI) GetBlockByHash(ctx context.Context, hash common.Hash, fullTx bool) (map[string]interface{}, error) {
 	block, err := api.b.BlockByHash(ctx, hash)
 	if block != nil {
+		block := PopulatePreGingerbreadBlockFields(ctx, api.b, block)
 		return RPCMarshalBlock(ctx, block, true, fullTx, api.b.ChainConfig(), api.b)
 	}
 	return nil, err
@@ -906,6 +910,7 @@ func (api *BlockChainAPI) GetUncleByBlockNumberAndIndex(ctx context.Context, blo
 			return nil, nil
 		}
 		block = types.NewBlockWithHeader(uncles[index])
+		block = PopulatePreGingerbreadBlockFields(ctx, api.b, block)
 		return RPCMarshalBlock(ctx, block, false, false, api.b.ChainConfig(), api.b)
 	}
 	return nil, err
@@ -921,6 +926,7 @@ func (api *BlockChainAPI) GetUncleByBlockHashAndIndex(ctx context.Context, block
 			return nil, nil
 		}
 		block = types.NewBlockWithHeader(uncles[index])
+		block = PopulatePreGingerbreadBlockFields(ctx, api.b, block)
 		return RPCMarshalBlock(ctx, block, false, false, api.b.ChainConfig(), api.b)
 	}
 	return nil, err
