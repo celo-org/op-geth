7f8a05a151ea1fbfe5ce45d4ce1cd1e15ac5379b
Karl Bartel <karl.bartel@clabs.co>
2025-01-24 11:40:00 +0100

    Read IntrinsicGas from FeeCurrencyDirectory  (#178)
    * Add FeeCurrencyContext object to BlockContext
    
    This replaces the `BlockContext.ExchangeRates` with a wrapper object
    `BlockContext.FeeCurrencyContext`.
    
    This enables easier addition of fee-currency related information
    to the block context and passing around that information.
    The exchange-rates are now part of the `FeeCurrencyContext`
    
    * Add intrinsic gas retrieval from FeeCurrencyDirectory
    
    * Use intrinsic gas from FeeCurrencyDirectory in STF
    
    * Add test for intrinsic gas too high in fee currency
    
    * Add empty fee-currency context to tools and tests
    
    * Add minor code improvements
    
    Co-authored-by: Paul Lange <palango@users.noreply.github.com>
    
    * Add comment for nil fee-currency code path
    
    * Add fee-currency-context to CeloBackend.NewEVM constructor
    
    ---------
    
    Co-authored-by: Paul Lange <palango@users.noreply.github.com>

--- eth/tracers/api_test.go
+++ eth/tracers/api_test.go
@@ -256,7 +256,7 @@ func (b *testBackend) StateAtTransaction(ctx context.Context, block *types.Block
 		if idx == txIndex {
 			return tx, context, statedb, release, nil
 		}
-		msg, _ := core.TransactionToMessage(tx, signer, block.BaseFee(), context.ExchangeRates)
+		msg, _ := core.TransactionToMessage(tx, signer, block.BaseFee(), context.FeeCurrencyContext.ExchangeRates)
 		if _, err := core.ApplyMessage(evm, msg, new(core.GasPool).AddGas(tx.Gas())); err != nil {
 			return nil, vm.BlockContext{}, nil, nil, fmt.Errorf("transaction %#x failed: %v", tx.Hash(), err)
 		}
