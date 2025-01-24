bb9a414cf55ff87bbe7bb190d633a063c6ae4831
kourin <kourin.code@gmail.com>
2025-01-24 23:11:41 +0900

    Add tests for Celo2 Signer compatibility with both Celo1 and Celo2 transactions (#308)
    * Fix typo
    
    * Add unit tests for forks
    
    * Add unit tests for Celo signer
    
    * Format code
    
    * Simplify Test_forks_activeForks and Test_forks_findTxFuncs
    
    * Remove Test_forks_findTxFuncs

--- core/types/celo_transaction_signing_forks.go
+++ core/types/celo_transaction_signing_forks.go
@@ -96,11 +96,11 @@ func (c *celoLegacy) txFuncs(tx *Transaction) *txFuncs {
 		return celoLegacyTxFuncs
 	case t == DynamicFeeTxType:
 		// We handle the dynamic fee tx type here because we need to handle
-		// migrated dynamic fee txs. These were enabeled in celo in the Espresso
+		// migrated dynamic fee txs. These were enabled in celo in the Espresso
 		// hardfork, which doesn't have any analogue in op-geth. Even though
 		// op-geth does enable support for dynamic fee txs in the London
 		// hardfork (which we set to the cel2 block) that fork contains a lot of
-		// changes that were not part of Espresso. So instead we ned to handle
+		// changes that were not part of Espresso. So instead we need to handle
 		// DynamicFeeTxTypes here.
 		return dynamicFeeTxFuncs
 	case t == AccessListTxType:
