0052b7ed34405a61ad5825b4da61b0bb94e77b3d
Karl Bartel <karl.bartel@clabs.co>
2025-03-19 16:28:38 +0100

    e2e: support large numbers in token duality test
    bash can't handle large integers correctly, so use perl for calculation.

--- e2e_test/test_token_duality.sh
+++ e2e_test/test_token_duality.sh
@@ -8,5 +8,12 @@ source shared.sh
 balance_before=$(cast balance 0x000000000000000000000000000000000000dEaD)
 cast send --private-key $ACC_PRIVKEY $TOKEN_ADDR 'transfer(address to, uint256 value) returns (bool)' 0x000000000000000000000000000000000000dEaD 100
 balance_after=$(cast balance 0x000000000000000000000000000000000000dEaD)
-echo "Balance change: $balance_before -> $balance_after"
-[[ $((balance_before + 100)) -eq $balance_after ]] || (echo "Balance did not change as expected"; exit 1)
+
+# Use perl for arbitrary precision arithmetic
+expected_balance=$(perl -e "use bignum; print $balance_before + 100")
+if [ "$expected_balance" != "$balance_after" ]; then
+    echo "Balance did not change as expected"
+    echo "Expected: $expected_balance"
+    echo "Actual: $balance_after"
+    exit 1
+fi
