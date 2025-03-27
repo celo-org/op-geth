08aa267aac4f5ade9c275a64229bac7ea34c25b7
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2025-03-27 17:23:52 +0100

    Add RW mutex to tx-ringbuffer

--- miner/tx_ringbuffer.go
+++ miner/tx_ringbuffer.go
@@ -1,6 +1,10 @@
 package miner
 
-import "github.com/ethereum/go-ethereum/common"
+import (
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+)
 
 // TxRingBuffer is a ring buffer of transaction hashes, it has one method to add
 // transaction hashes and another method to check if a transaction hash is in the buffer.
@@ -8,6 +12,7 @@ type TxRingBuffer struct {
 	ring  []common.Hash
 	m     map[common.Hash]struct{}
 	index int
+	mux   *sync.RWMutex
 }
 
 // NewTxRingBuffer creates a new transaction ring buffer with the given size.
@@ -15,12 +20,16 @@ func NewTxRingBuffer(size int) *TxRingBuffer {
 	return &TxRingBuffer{
 		ring: make([]common.Hash, size),
 		m:    make(map[common.Hash]struct{}),
+		mux:  &sync.RWMutex{},
 	}
 }
 
 // Add adds a new transaction hash to the ring buffer, if the buffer is
 // at capacity, the oldest transaction hash is overwritten.
 func (b *TxRingBuffer) Add(tx common.Hash) {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+
 	// Clean up old transaction hash from map
 	oldHash := b.ring[b.index]
 	delete(b.m, oldHash)
@@ -37,6 +46,9 @@ func (b *TxRingBuffer) Add(tx common.Hash) {
 }
 
 func (b *TxRingBuffer) Has(tx common.Hash) bool {
+	b.mux.RLock()
+	defer b.mux.RUnlock()
+
 	_, exists := b.m[tx]
 	return exists
 }
