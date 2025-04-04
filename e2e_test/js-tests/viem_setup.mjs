6b8a5ed9237cc605078c829ecbbce928531be42b
Gastón Ponti <gaston.ponti@clabs.co>
2025-04-04 07:25:03 -0300

    Fix webSocket configuration

--- e2e_test/js-tests/viem_setup.mjs
+++ e2e_test/js-tests/viem_setup.mjs
@@ -4,6 +4,7 @@ import {
 	createPublicClient,
 	createWalletClient,
 	http,
+	webSocket,
 	defineChain,
 } from "viem";
 import { celo, celoAlfajores } from "viem/chains";
@@ -16,6 +17,7 @@ const devChain = defineChain({
 	name: "local dev chain",
 	rpcUrls: {
 		default: {
+			http: [process.env.ETH_RPC_URL],
 			webSocket: [process.env.ETH_RPC_URL],
 		},
 	},
@@ -27,6 +29,7 @@ const celoBaklava = defineChain({
 	name: "baklava",
 	rpcUrls: {
 		default: {
+			http: [process.env.ETH_RPC_URL],
 			webSocket: [process.env.ETH_RPC_URL],
 		},
 	},
@@ -36,6 +39,7 @@ const celoMainnet = defineChain({
 	...celo,
 	rpcUrls: {
 		default: {
+			http: [process.env.ETH_RPC_URL],
 			webSocket: [process.env.ETH_RPC_URL],
 		},
 	},
@@ -54,14 +58,25 @@ const chain = (() => {
 	};
 })();
 
+const transportForNetwork = (() => {
+	switch (process.env.NETWORK) {
+		case 'alfajores':
+		case 'baklava':
+		case 'mainnet':
+			return http()
+		default:
+			return http()
+	};
+})
+
 // Set up clients/wallet
 export const publicClient = createPublicClient({
 	chain: chain,
-	transport: http(),
+	transport: transportForNetwork(),
 });
 export const account = privateKeyToAccount(process.env.ACC_PRIVKEY);
 export const walletClient = createWalletClient({
 	account,
 	chain: chain,
-	transport: http(),
+	transport: transportForNetwork(),
 });
