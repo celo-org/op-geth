5dd4136df43955a6aa702d88742eb25199473668
Piers Powlesland <pierspowlesland@gmail.com>
2025-04-07 13:02:08 +0100

    Add explanatory comments

--- miner/currency_blocklist_test.go
+++ miner/currency_blocklist_test.go
@@ -71,7 +71,10 @@ func TestBlocklistAddAfterEviction(t *testing.T) {
 func TestBlocklistRemove(t *testing.T) {
 	bl := NewAddressBlocklist()
 
+	// Check that removing a fee currency  from an empty blocklist doesn't panic.
 	bl.Remove(feeCurrency1)
+
+	// Check that removal of existing fee currency works.
 	bl.Add(feeCurrency1, header)
 	bl.Add(feeCurrency2, header)
 	bl.Remove(feeCurrency1)
