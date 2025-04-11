7499b227beab9aa09a533ebf664862eb993b45af
Piers Powlesland <pierspowlesland@gmail.com>
2025-04-11 21:36:00 +0100

    rename internal/ethapi/api_test.go

--- internal/ethapi/api_test.go
+++ internal/ethapi/api_test.go
@@ -3641,7 +3641,7 @@ func TestCeloTransaction_RoundTripRpcJSON(t *testing.T) {
 
 func celoTransactionTypes(addr common.Address, config *params.ChainConfig) []types.TxData {
 	return []types.TxData{
-		&types.CeloDynamicFeeTx{
+		&types.CeloDynamicFeeTxV2{
 			ChainID:     config.ChainID,
 			Nonce:       5,
 			GasTipCap:   big.NewInt(6),
@@ -3661,7 +3661,7 @@ func celoTransactionTypes(addr common.Address, config *params.ChainConfig) []typ
 			R: big.NewInt(10),
 			S: big.NewInt(11),
 		},
-		&types.CeloDynamicFeeTx{
+		&types.CeloDynamicFeeTxV2{
 			ChainID:     config.ChainID,
 			Nonce:       5,
 			GasTipCap:   big.NewInt(6),
