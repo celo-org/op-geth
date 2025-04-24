1d15692bd926befd0590f3d3ca21f034a382d23c
Piers Powlesland <pierspowlesland@gmail.com>
2025-04-24 20:45:34 +0100

    Refactor and add e2e tests, run on alfajores (#212,#258,#268,#293)
    Refactors debug fee currency deployment ensuring fee currency cleanup. The
    geth-devmode predeployed "oracle3" is reused throughout the e2e tests
    for all dynamically deployed fee-currencies. However the tested
    fee-currencies are never removed from the FeeCurrencyDirectory.
    
    This causes the node to try to query exchange-rates for the leftover
    fee-currencies. This fails, because the oracle associated with the old
    fee-currency has a different token address assigned and thus causes a
    revert. This issue is not critical and only causes emission of a log
    message and some unneccessary evm calls,
    but it is better practice to clean up the fee-currency from the
    directory anyways.
    
    Also adds a test of transactions that exceed the max intrinsic gas for a fee
    currency, this checks that the transaction fails and the fee currency is
    blocked.
    
    Allows tests to be run on Alfajores. This entailed increasing gas limits
    for a number of transactions that interact with fee currencies, since
    the fee currencies on alfajores are different to our debug ones and also
    tests needed to be updated to not rely on hardcoded fee currency rates.
    
    Also adds tests for fee currency use with the gas price RPC API
    
    Skips test_fee_currency_fails_on_credit.sh as it seems to be broken.

--- e2e_test/test_fee_currency_fails_intrinsic.sh
+++ e2e_test/test_fee_currency_fails_intrinsic.sh
@@ -5,16 +5,17 @@ set -eo pipefail
 source shared.sh
 source debug-fee-currency/lib.sh
 
-tail -F -n0 geth.log >debug-fee-currency/geth.partial.log & # start log capture
+# Expect that the creditGasFees failed and is logged by geth
+tail -F -n 0 geth.log >debug-fee-currency/geth.intrinsic.log & # start log capture
 trap 'kill %%' EXIT # kill bg tail job on exit
 (
 	sleep 0.2
-	fee_currency=$(deploy_fee_currency false true false)
+	fee_currency=$(deploy_fee_currency false false true)
 
 	# trigger the first failed call to the CreditFees(), causing the
 	# currency to get temporarily blocklisted.
 	# initial tx should not succeed, should have required a replacement transaction.
-	cip_64_tx $fee_currency 1 true | assert_cip_64_tx false
+	cip_64_tx $fee_currency 1 true 2 | assert_cip_64_tx false
 
 	sleep 2
 
@@ -22,12 +23,11 @@ trap 'kill %%' EXIT # kill bg tail job on exit
 	# this should NOT make the transaction execute anymore,
 	# but invalidate the transaction earlier.
 	# initial tx should not succeed, should have required a replacement transaction.
-	cip_64_tx $fee_currency 1 true | assert_cip_64_tx false
+	cip_64_tx $fee_currency 1 true 2 | assert_cip_64_tx false
 
 	cleanup_fee_currency $fee_currency
 )
 sleep 0.5
 # although we sent a transaction wih faulty fee-currency twice,
 # the EVM call should have been executed only once
-grep "" debug-fee-currency/geth.partial.log
-if [ "$(grep -Ec "fee-currency EVM execution error, temporarily blocking fee-currency in local txpools .+ This DebugFeeCurrency always fails in \(old\) creditGasFees!" debug-fee-currency/geth.partial.log)" -ne 1 ]; then exit 1; fi
+if [ "$(grep -Ec "fee-currency EVM execution error, temporarily blocking fee-currency in local txpools .+ surpassed maximum allowed intrinsic gas for CreditFees\(\) in fee-currency" debug-fee-currency/geth.intrinsic.log)" -ne 1 ]; then exit 1; fi
