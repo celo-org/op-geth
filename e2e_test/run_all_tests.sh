24be2664d8cc08d49dddf41a47748c602c74e58e
Piers Powlesland <pierspowlesland@gmail.com>
2025-04-24 16:42:22 +0100

    Refactor e2e tests and run on alfajores (#212)
    * Revert "Temporarily skip fee-currency e2e tests"
    
    This reverts commit 25314bcb23a9bcc18be274e7c0df320daa2d7215.
    
    * Refactor e2e tests
    
    Since some e2e tests now produce invalidated transactions that remain
    in the txpool until replaced, the `send_tx.mjs` script required some
    changes to replace the sent CIP64 transaction. It will get replaced
    after 2 blocks of producing no receipt.
    Those changes also required the geth-node to run in periodic
    --dev mode instead of the on-demand "produce-block-per-transaction"
    mode, which
    will unfortunately make the tests run longer than before.
    
    The bash scripts and tests itself have also been refactored
    to make code segments more reusable with the help of functions.
    
    * Fix js waitBlocks
    
    * Fix e2e oracle reuse causing exchangeRate error
    
    The geth-devmode predeployed "oracle3" is reused throughout the e2e
    tests for all dynamically deployed fee-currencies.
    However the tested fee-currencies are never removed from the
    FeeCurrencyDirectory.
    
    This causes the node to try to query exchange-rates for the leftover
    fee-currencies. This fails, because the oracle associated with the old
    fee-currency has a different token address assigned and thus causes a
    revert. This issue is not critical and only causes emission of a log
    message and some unneccessary evm calls,
    but it is better practice to clean up the fee-currency from the
    directory anyways.
    
    * Remove accidentally checked in comment
    
    * Reintroduce geth insta-mine in e2e tests
    
    * Add replacement transaction related arguments to send_tx
    
    * Add gas price difference test
    
    Run e2e tests on alfajores (#258)
    
    Modifies our e2e tests to be able to run on alfajores if the
    environment variable `NETWORK` is set to 'alfajores'.
    Also adds a CI step to run them against alfajores.
    
    Note that the `test_fee_currency...` tests are skipped
    because they require the ability to deploy new fee currencies,
    which we probably don't want to be doing on a live network
    as it could cause issues for people, and we also don't want to
    expose the credentials to do so.
    
    Set correct fee currency directory in e2e test
    
    Skip failing tests (#268)
    
    Fix failing e2e tests in test_viem_tx (#293)
    
    * Fix failing e2e tests in test_viem_tx
    
    * Add timeout for some failing tests
    
    * Add comments about new sleep in e2e test
    
    * Add sleep in test_token_duality
    
    * Revert adding sleep in e2e tests
    
    * Fix price coefficient of tx which can't be replaced in test_viem_tx e2e test
    
    Use fee currency in min tip check e2e fixup
    
    Fix just high enough test
    
    Skip test_fee_currency_fails_on_credit.sh

--- e2e_test/run_all_tests.sh
+++ e2e_test/run_all_tests.sh
@@ -2,54 +2,65 @@
 set -eo pipefail
 
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
+source "$SCRIPT_DIR/shared.sh"
+
 TEST_GLOB=$1
 
-## Start geth
-cd "$SCRIPT_DIR/.." || exit 1
-make geth
-trap 'kill %%' EXIT  # kill bg job at exit
-build/bin/geth --dev --http --http.api eth,web3,net &> "$SCRIPT_DIR/geth.log" &
+if [ -z $NETWORK ]; then
+    ## Start geth
+    cd "$SCRIPT_DIR/.." || exit 1
+    make geth
+    trap 'kill %%' EXIT # kill bg job at exit
+    build/bin/geth --dev --http --http.api eth,web3,net --txpool.nolocals &>"$SCRIPT_DIR/geth.log" &
+    
+    # Wait for geth to be ready
+    for _ in {1..10}; do
+    	if cast block &>/dev/null; then
+    		break
+    	fi
+    	sleep 0.2
+    done
+    
+    ## Run tests
+    echo Geth ready, start tests
+fi
 
-# Wait for geth to be ready
-for _ in {1..10}
-do
-	if cast block &> /dev/null
-	then
-		break
-	fi
-	sleep 0.2
-done
-
-## Run tests
-echo Geth ready, start tests
 cd "$SCRIPT_DIR" || exit 1
-
 # There's a problem with geth return errors on the first transaction sent.
 # See https://github.com/ethereum/web3.py/issues/3212
 # To work around this, send a transaction before running tests
-source ./shared.sh
-cast send --json --private-key $ACC_PRIVKEY $TOKEN_ADDR 'transfer(address to, uint256 value) returns (bool)' 0x000000000000000000000000000000000000dEaD 100 --async
+cast send --json --private-key "$ACC_PRIVKEY" "$TOKEN_ADDR" 'transfer(address to, uint256 value) returns (bool)' 0x000000000000000000000000000000000000dEaD 100 > /dev/null || true
 
 failures=0
 tests=0
-for f in test_*"$TEST_GLOB"*
-do
-	# temporarily skip fee-currency related tests
-	if (echo "$f" |  grep -q -e fee_currency )
-	then
-		tput setaf 2 || true
-		echo "SKIP $f"
+echo "Globbing with \"$TEST_GLOB\""
+for f in test_*"$TEST_GLOB"*; do
+	echo "for file $f"
+	if [[ -n $NETWORK ]]; then
+		case $f in
+		  # Skip tests that require a local network.
+		  test_fee_currency_fails_on_credit.sh|test_fee_currency_fails_on_debit.sh|test_fee_currency_fails_intrinsic.sh)
+		  echo "skipping file $f"
+		  continue
+		  ;;
+	    esac
 	else
-		echo -e "\nRun $f"
-		if "./$f"
-		then
-			tput setaf 2 || true
-			echo "PASS $f"
-		else
-			tput setaf 1 || true
-			echo "FAIL $f ❌"
-			((failures++)) || true
-		fi
+		case $f in
+		  # Skip test fee currency fails on credit, it seems broken
+		  test_fee_currency_fails_on_credit.sh)
+		  echo "skipping file $f"
+		  continue
+		  ;;
+	    esac
+	fi
+	echo -e "\nRun $f"
+	if "./$f"; then
+		tput setaf 2 || true
+		echo "PASS $f"
+	else
+		tput setaf 1 || true
+		echo "FAIL $f ❌"
+		((failures++)) || true
 	fi
 	tput sgr0 || true
 	((tests++)) || true
@@ -57,8 +68,7 @@ done
 
 ## Final summary
 echo
-if [[ $failures -eq 0 ]] 
-then
+if [[ $failures -eq 0 ]]; then
 	tput setaf 2 || true
 	echo All $tests tests succeeded!
 else
