089d6b2c25850e24329cfb3b8b9516e9680e0816
protolambda <proto@protolambda.com>
2025-05-23 11:43:25 +0200

    build: Update EEST to v4.5.0 (ethereum/go-ethereum#31880) (#612)
    We deleted outdated pectra-devnet-6@v1.0.0 release by mistake, so this
    PR updates the referenced EEST release to the correct latest version.
    
    @s1na I removed the TODO comment because I think this solves it, unless
    it meant something else.
    
    ---------
    
    Co-authored-by: Mario Vega <marioevz@gmail.com>
    Co-authored-by: MariusVanDerWijden <m.vanderwijden@live.de>

--- tests/block_test.go
+++ tests/block_test.go
@@ -79,6 +79,9 @@ func TestExecutionSpecBlocktests(t *testing.T) {
 	}
 	bt := new(testMatcher)
 
+	bt.skipLoad(".*prague/eip7251_consolidations/contract_deployment/system_contract_deployment.json")
+	bt.skipLoad(".*prague/eip7002_el_triggerable_withdrawals/contract_deployment/system_contract_deployment.json")
+
 	bt.walk(t, executionSpecBlockchainTestDir, func(t *testing.T, name string, test *BlockTest) {
 		matches, err := regexp.MatchString("blockchain_test-create2?-over_limit_(ones|zeros)", name)
 		if err != nil {
