145febd96e7f48cfa6b5c04b7feb668e254f7821
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2025-05-27 14:27:31 +0200

    Adapt viem e2e test's asserts to upstream fix
    Upstream viem recently merged a fix to the `estimateFeesPerGas`
    function (https://github.com/wevm/viem/pull/3117),
    and the recent updates to the e2e tests npm packages now includes the
    fixed version of viem.
    
    With this the asserted values in the viem e2e test are no longer a
    reflection of what es expected to be returned from the viem library
    and had to be adjusted.

--- e2e_test/js-tests/test_viem_tx.mjs
+++ e2e_test/js-tests/test_viem_tx.mjs
@@ -160,20 +160,11 @@ describe("viem send tx", () => {
 		});
 
 		// Get the exchange rates for the fee currency.
-		const abi = parseAbi(['function getExchangeRate(address token) public view returns (uint256 numerator, uint256 denominator)']);
-		const [numerator, denominator] = await publicClient.readContract({
-			address: process.env.FEE_CURRENCY_DIRECTORY_ADDR,
-			abi: abi,
-			functionName: 'getExchangeRate',
-			args: [process.env.FEE_CURRENCY],
-		});
+		const rate = await getRate(process.env.FEE_CURRENCY);
 
-		// TODO fix this when viem is fixed - https://github.com/celo-org/viem/pull/20
-		// The expected value for the max fee should be the (baseFeePerGas * multiplier) + maxPriorityFeePerGas
-		// Instead what is currently returned is (maxFeePerGas * multiplier) + maxPriorityFeePerGas
-		const maxPriorityFeeInFeeCurrency = (maxPriorityFeePerGasNative * numerator) / denominator;
-		const maxFeeInFeeCurrency = ((block.baseFeePerGas + maxPriorityFeePerGasNative) * numerator) / denominator;
-		assert.equal(fees.maxFeePerGas, ((maxFeeInFeeCurrency * 12n) / 10n) + maxPriorityFeeInFeeCurrency);
+		const maxPriorityFeeInFeeCurrency = rate.toFeeCurrency(maxPriorityFeePerGasNative);
+		const baseFeeInFeeCurrency = rate.toFeeCurrency(block.baseFeePerGas);
+		assert.equal(fees.maxFeePerGas, ((baseFeeInFeeCurrency * 12n) / 10n) + maxPriorityFeeInFeeCurrency);
 		assert.equal(fees.maxPriorityFeePerGas, maxPriorityFeeInFeeCurrency);
 
 		// check that the prepared transaction request uses the
