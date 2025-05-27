8749c7ffeda01bb3021bfc1f9ae2922e393fa314
Paul Lange <palango@gmx.de>
2025-05-27 16:12:46 +0200

    isthmus: Add hardfork timestamps

--- params/celo.go
+++ params/celo.go
@@ -2,4 +2,8 @@ package params
 
 const (
 	DefaultGasLimit uint64 = 20000000 // Gas limit of the blocks before BlockchainParams contract is loaded.
+
+	BaklavaIsthmusTimestamp     uint64 = 1749049861 // Wed Jun 04 2025 15:11:01 GMT+0000
+	AlfajoresIsthmusTimestamp   uint64 = 1749654322 // Wed Jun 11 2025 15:05:22 GMT+0000
+	CeloMainnetIsthmusTimestamp uint64 = 1750863765 // Wed Jun 25 2025 15:02:45 GMT+0000
 )
