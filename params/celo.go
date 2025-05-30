071ac94dc7e4fecd58fbddc831ced56dca6e9a53
Gastón Ponti <gaston.ponti@clabs.co>
2025-05-30 17:36:02 -0300

    isthmus: Update hardfork timestamps

--- params/celo.go
+++ params/celo.go
@@ -3,7 +3,7 @@ package params
 const (
 	DefaultGasLimit uint64 = 20000000 // Gas limit of the blocks before BlockchainParams contract is loaded.
 
-	BaklavaIsthmusTimestamp     uint64 = 1749049861 // Wed Jun 04 2025 15:11:01 GMT+0000
-	AlfajoresIsthmusTimestamp   uint64 = 1749654322 // Wed Jun 11 2025 15:05:22 GMT+0000
-	CeloMainnetIsthmusTimestamp uint64 = 1750863765 // Wed Jun 25 2025 15:02:45 GMT+0000
+	BaklavaIsthmusTimestamp     uint64 = 1749654000 // Wed Jun 11 2025 15:00:00 GMT+0000
+	AlfajoresIsthmusTimestamp   uint64 = 1750863600 // Wed Jun 25 2025 15:00:00 GMT+0000
+	CeloMainnetIsthmusTimestamp uint64 = 1752073200 // Wed Jul 09 2025 15:00:00 GMT+0000
 )
