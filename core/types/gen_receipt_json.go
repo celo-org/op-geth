0d6f42279f5c96cdab0c8c791779f1d67e725f7c
kourin <kourin.code@gmail.com>
2025-08-04 20:55:44 +0900

    rpc: Includes baseFee in receipt of RPC response (#347)
    * include baseFee in JSON-formatted receipt
    
    * Add tests for serializing receipt in JSON-RPC API
    
    * Add tests for serializing receipt in JSON-RPC API
    
    * Omit baseFee field for CeloDynamicFeeTxV2 in pre Cel2
    
    * Add TestHashConsistency
    
    * Fix comment
    
    * Remove unnecessary context cancel call
    
    * Fix status logging in TestHashConsistency
    
    * Fix log
    
    * Add Bloom Test in TestHashConsistency
    
    chore: Update checksum (#386)
    
    * chore: Update checksum
    
    * chore: Run `go generate`
    
    Update core/tracing/gen_balance_change_reason_stringer.go

--- core/types/gen_receipt_json.go
+++ core/types/gen_receipt_json.go
@@ -42,7 +42,7 @@ func (r Receipt) MarshalJSON() ([]byte, error) {
 		L1BlobBaseFeeScalar   *hexutil.Uint64 `json:"l1BlobBaseFeeScalar,omitempty"`
 		OperatorFeeScalar     *hexutil.Uint64 `json:"operatorFeeScalar,omitempty"`
 		OperatorFeeConstant   *hexutil.Uint64 `json:"operatorFeeConstant,omitempty"`
-		BaseFee               *big.Int        `json:"baseFee,omitempty"`
+		BaseFee               *hexutil.Big    `json:"baseFee,omitempty"`
 	}
 	var enc Receipt
 	enc.Type = hexutil.Uint64(r.Type)
@@ -69,13 +69,9 @@ func (r Receipt) MarshalJSON() ([]byte, error) {
 	enc.FeeScalar = r.FeeScalar
 	enc.L1BaseFeeScalar = (*hexutil.Uint64)(r.L1BaseFeeScalar)
 	enc.L1BlobBaseFeeScalar = (*hexutil.Uint64)(r.L1BlobBaseFeeScalar)
-	if r.OperatorFeeScalar != nil {
-		enc.OperatorFeeScalar = (*hexutil.Uint64)(r.OperatorFeeScalar)
-	}
-	if r.OperatorFeeConstant != nil {
-		enc.OperatorFeeConstant = (*hexutil.Uint64)(r.OperatorFeeConstant)
-	}
-	enc.BaseFee = r.BaseFee
+	enc.OperatorFeeScalar = (*hexutil.Uint64)(r.OperatorFeeScalar)
+	enc.OperatorFeeConstant = (*hexutil.Uint64)(r.OperatorFeeConstant)
+	enc.BaseFee = (*hexutil.Big)(r.BaseFee)
 	return json.Marshal(&enc)
 }
 
@@ -108,7 +104,7 @@ func (r *Receipt) UnmarshalJSON(input []byte) error {
 		L1BlobBaseFeeScalar   *hexutil.Uint64 `json:"l1BlobBaseFeeScalar,omitempty"`
 		OperatorFeeScalar     *hexutil.Uint64 `json:"operatorFeeScalar,omitempty"`
 		OperatorFeeConstant   *hexutil.Uint64 `json:"operatorFeeConstant,omitempty"`
-		BaseFee               *big.Int        `json:"baseFee,omitempty"`
+		BaseFee               *hexutil.Big    `json:"baseFee,omitempty"`
 	}
 	var dec Receipt
 	if err := json.Unmarshal(input, &dec); err != nil {
@@ -198,7 +194,7 @@ func (r *Receipt) UnmarshalJSON(input []byte) error {
 		r.OperatorFeeConstant = (*uint64)(dec.OperatorFeeConstant)
 	}
 	if dec.BaseFee != nil {
-		r.BaseFee = dec.BaseFee
+		r.BaseFee = (*big.Int)(dec.BaseFee)
 	}
 	return nil
 }
