86aecaa416bbfa597abba5c4417b57810d3a2ae0
kourin <kourin.code@gmail.com>
2025-08-04 20:55:44 +0900

    rpc: Includes baseFee in receipt of RPC response (#347)
    * include baseFee in JSON-formatted receipt
    
    * Add tests for serializing receipt in JSON-RPC API
    
    * Add tests for serializing receipt in JSON-RPC API
    
    * Omit baseFee field for CeloDynamicFeeTxV2 in pre Cel2
    
    * Add TestHashConsistency
    
    * Fix comment
    
    * Remove unnecessary context cancel call
    
    * Fix status logging in TestHashConsistency
    
    * Fix log
    
    * Add Bloom Test in TestHashConsistency
    
    chore: Update checksum (#386)
    
    * chore: Update checksum
    
    * chore: Run `go generate`
    
    Update core/tracing/gen_balance_change_reason_stringer.go

--- core/types/gen_receipt_json.go
+++ core/types/gen_receipt_json.go
@@ -43,8 +43,9 @@ func (r Receipt) MarshalJSON() ([]byte, error) {
 		OperatorFeeScalar     *hexutil.Uint64 `json:"operatorFeeScalar,omitempty"`
 		OperatorFeeConstant   *hexutil.Uint64 `json:"operatorFeeConstant,omitempty"`
 		DAFootprintGasScalar  *hexutil.Uint64 `json:"daFootprintGasScalar,omitempty"`
+
 		// Celo-specific receipt field
-		BaseFee *big.Int `json:"baseFee,omitempty"`
+		BaseFee *hexutil.Big `json:"baseFee,omitempty"`
 	}
 	var enc Receipt
 	enc.Type = hexutil.Uint64(r.Type)
@@ -76,7 +77,7 @@ func (r Receipt) MarshalJSON() ([]byte, error) {
 	enc.DAFootprintGasScalar = (*hexutil.Uint64)(r.DAFootprintGasScalar)
 
 	// Celo-specific receipt field
-	enc.BaseFee = r.BaseFee
+	enc.BaseFee = (*hexutil.Big)(r.BaseFee)
 
 	return json.Marshal(&enc)
 }
@@ -111,7 +112,7 @@ func (r *Receipt) UnmarshalJSON(input []byte) error {
 		OperatorFeeScalar     *hexutil.Uint64 `json:"operatorFeeScalar,omitempty"`
 		OperatorFeeConstant   *hexutil.Uint64 `json:"operatorFeeConstant,omitempty"`
 		DAFootprintGasScalar  *hexutil.Uint64 `json:"daFootprintGasScalar,omitempty"`
-		BaseFee               *big.Int        `json:"baseFee,omitempty"`
+		BaseFee               *hexutil.Big    `json:"baseFee,omitempty"`
 	}
 	var dec Receipt
 	if err := json.Unmarshal(input, &dec); err != nil {
@@ -204,7 +205,7 @@ func (r *Receipt) UnmarshalJSON(input []byte) error {
 		r.DAFootprintGasScalar = (*uint64)(dec.DAFootprintGasScalar)
 	}
 	if dec.BaseFee != nil {
-		r.BaseFee = dec.BaseFee
+		r.BaseFee = (*big.Int)(dec.BaseFee)
 	}
 	return nil
 }
