b5127267e7814d5e750bff576c22f2e6eea14d1e
piersy <pierspowlesland@gmail.com>
2025-08-27 12:44:54 +0100

    Remove initing genesis hack (#432)
    The initing genesis flag was added to address a problem in the `TestFeeHistory`
    test to allow it to construct post gingerbread genesis blocks while also
    setting the gingerbread block.
    
    Without this, the test would fail because pre-gingerbread blocks did not have a
    gas limit field and would thus fail the header verification used in the test.
    
    `InitingGenesis` was checked in the below condition in the genesis
    `toBlockWithRoot` method to limit pre gingerbread header construction to just
    the flow when we are initing genesis, thus allowing `TestFeeHistory` to always
    construct post gingerbread genesis blocks.
    
    if g.InitingGenesis() && g.Config.GingerbreadBlock != nil && !g.Config.IsGingerbread(number) {
        head = types.NewPreGingerbreadHeader()
    }
    
    However it turns out we don't need to set the gingerbread block at all for this
    test to pass, and so not setting the field also causes this check to be
    skipped, resulting in all post gingerbread genesis blocks.
    
    The condition should still trigger when we are initing genesis when running a
    snap sync node, because the gingerbread block will be set.

--- eth/gasprice/gasprice_test.go
+++ eth/gasprice/gasprice_test.go
@@ -166,7 +166,6 @@ func newTestBackend(t *testing.T, londonBlock *big.Int, cancunBlock *big.Int, pe
 		}
 	}
 	config.LondonBlock = londonBlock
-	config.GingerbreadBlock = londonBlock
 	config.ArrowGlacierBlock = londonBlock
 	config.GrayGlacierBlock = londonBlock
 	if cancunBlock != nil {
