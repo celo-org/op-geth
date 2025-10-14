c4e08fadbd2d90dd1cba959afbae5b5cee29a682
Karl Bartel <karl.bartel@clabs.co>
2025-10-14 12:54:58 +0200

    precompile: Prevent from running in STATICCALLs
    See https://github.com/celo-org/celo-blockchain-planning/issues/1261

--- core/vm/celo_contracts.go
+++ core/vm/celo_contracts.go
@@ -59,6 +59,11 @@ func (c *transfer) RequiredGas(input []byte) uint64 {
 }
 
 func (c *transfer) Run(input []byte, ctx *celoPrecompileContext) ([]byte, error) {
+	// STATICCALL protection: prevent state changes in read-only context
+	if ctx.evm.interpreter.readOnly {
+		return nil, ErrWriteProtection
+	}
+
 	if isCeloToken, err := ctx.IsCallerCeloToken(); err != nil {
 		return nil, err
 	} else if !isCeloToken {
--- core/vm/celo_contracts_test.go
+++ core/vm/celo_contracts_test.go
@@ -60,6 +60,10 @@ var mockEVM = &EVM{
 	TxContext:   vmTxCtx,
 }
 
+func init() {
+	mockEVM.interpreter = NewEVMInterpreter(mockEVM)
+}
+
 var mockPrecompileContext = NewContext(common.HexToAddress("1337"), mockEVM)
 
 func TestPrecompileTransfer(t *testing.T) {
@@ -117,4 +121,31 @@ func TestPrecompileTransfer(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestPrecompileTransferStaticCallProtection(t *testing.T) {
+	// Input with zero from address (will fail balance check in non-static context)
+	validInput := make([]byte, 96)
+	validInput[63] = 1   // to address
+	validInput[95] = 100 // value
+
+	t.Run("rejects transfer in static context", func(t *testing.T) {
+		mockEVM.interpreter.readOnly = true
+		defer func() { mockEVM.interpreter.readOnly = false }()
+
+		ctx := NewContext(addresses.MainnetAddresses.CeloToken, mockEVM)
+		_, err := (&transfer{}).Run(validInput, ctx)
+		if err != ErrWriteProtection {
+			t.Errorf("expected ErrWriteProtection, got %v", err)
+		}
+	})
+
+	t.Run("allows transfer in non-static context", func(t *testing.T) {
+		ctx := NewContext(addresses.MainnetAddresses.CeloToken, mockEVM)
+		_, err := (&transfer{}).Run(validInput, ctx)
+		// Should fail with insufficient balance, not write protection
+		if err != ErrInsufficientBalance {
+			t.Errorf("expected ErrInsufficientBalance, got %v", err)
+		}
+	})
 }
