408856bc03c5b80c3a00a6d48f3c76b429d70f30
Karl Bartel <karl.bartel@clabs.co>
2025-11-03 12:16:39 +0100

    isthmus: Add hardfork timestamps (#388, #395)
    Co-authored-by: Paul Lange <palango@gmx.de>
    
    Overrride Holosene and Isthmus activation time even if config.Genesis is nil (#395)
    
    * Overrride Holosene and Isthmus activation time even if config.Genesis is nil
    
    isthmus: Update hardfork timestamps (#390)

--- params/celo.go
+++ params/celo.go
@@ -2,4 +2,6 @@ package params
 
 const (
 	DefaultGasLimit uint64 = 20000000 // Gas limit of the blocks before BlockchainParams contract is loaded.
+
+	CeloMainnetIsthmusTimestamp uint64 = 1752073200 // Wed Jul 09 2025 15:00:00 GMT+0000
 )
