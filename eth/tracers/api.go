bestyourwallet/celo-geth

gasestimator: Support fee currencies in gas estimation and gas price RPC APIs
Updates eth_gasPrice, eth_maxPriorityFeePerGas and eth_estimateGas to
take into account fee currencies, updates ehtclient calls to match.

Also adds the concept of fee currency to the TransactionArgs struct is used
in transaction related endpoints like `eth_sendTransaction`,
`eth_signTransaction`, `eth_estimateGas` and many more.

eth/tracers/api.go
@@ -1030,7 +1030,7 @@ func (api *API) TraceCall(ctx context.Context, args ethapi.TransactionArgs, bloc
 		return nil, err
 	}
 	var (
-		msg         = args.ToMessage(vmctx.BaseFee, true, true)
+		msg         = args.ToMessage(vmctx.BaseFee, true, true, vmctx.ExchangeRates)
 		tx          = args.ToTransaction(types.LegacyTxType)
 		traceConfig *TraceConfig
 	)

celo-org/op-geth

eth/tracers/api.go
@@ -1041,7 +1041,7 @@ func (api *API) TraceCall(ctx context.Context, args ethapi.TransactionArgs, bloc
 		return nil, err
 	}
 	var (
-		msg         = args.ToMessage(blockContext.BaseFee, true, true)
+		msg         = args.ToMessage(blockContext.BaseFee, true, true, blockContext.ExchangeRates)
 		tx          = args.ToTransaction(types.LegacyTxType)
 		traceConfig *TraceConfig
 	)