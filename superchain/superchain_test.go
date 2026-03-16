3a40c398c038a67b0d8210f9bfc4950fba3da190
Paul Lange <palango@users.noreply.github.com>
2026-03-16 15:50:05 +0100

    chore: update superchain registry (#486)

--- superchain/superchain_test.go
+++ superchain/superchain_test.go
@@ -12,7 +12,7 @@ func TestGetSuperchain(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, "Mainnet", mainnet.Name)
-	require.Equal(t, common.HexToAddress("0x8062AbC286f5e7D9428a0Ccb9AbD71e50d93b935"), mainnet.ProtocolVersionsAddr)
+	require.Equal(t, common.HexToAddress("0x1b6dEB2197418075AB314ac4D52Ca1D104a8F663"), mainnet.ProtocolVersionsAddr)
 	require.EqualValues(t, 1, mainnet.L1.ChainID)
 
 	_, err = GetSuperchain("not a network")
