bestyourwallet/celo-geth

fee: Reinstate l1cost function for alfajores (#271, #376)
This commit reinstates a custom l1 cost function for Alfajores which always
returns zero.

We thought we could get rid of this by simply setting the l1 fee scalars to
zero, but we missed the l1 blob fee scalars on alfajores so removing our custom
l1 cost function causes a hardfork while syncing alfajores.

In order to remove our l1 cost function without introducing an unintentional
hardfork, we'll need to have a hardfork so that we can switch off the use of
the cost function at a certain point. But for now reinstating our custom cost
function means that we can use this branch for our alfajores deployment. This
saves us having to manage two separate branches (one for local development
and one to be deployed on alfajores).

Co-authored-by: kourin <kourin.code@gmail.com>

internal/ethapi/celo_api_test.go
@@ -384,7 +384,7 @@ func TestNewRPCTransactionDynamicFee(t *testing.T) {
 func allEnabledChainConfig() *params.ChainConfig {
 	zeroTime := uint64(0)
 	return &params.ChainConfig{
-		ChainID:             big.NewInt(44787),
+		ChainID:             big.NewInt(params.CeloAlfajoresChainID),
 		HomesteadBlock:      big.NewInt(0),
 		EIP150Block:         big.NewInt(0),
 		EIP155Block:         big.NewInt(0),

celo-org/op-geth

internal/ethapi/celo_api_test.go
@@ -384,7 +384,7 @@ func TestNewRPCTransactionDynamicFee(t *testing.T) {
 func allEnabledChainConfig() *params.ChainConfig {
 	zeroTime := uint64(0)
 	return &params.ChainConfig{
-		ChainID:             big.NewInt(44787),
+		ChainID:             big.NewInt(params.CeloSepoliaChainID),
 		HomesteadBlock:      big.NewInt(0),
 		EIP150Block:         big.NewInt(0),
 		EIP155Block:         big.NewInt(0),